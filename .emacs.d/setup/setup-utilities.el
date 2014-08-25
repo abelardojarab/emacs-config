776f71d21f9fa4f9b8e3a876e0e5df07ecfa593f
Abelardo Jara <abelardojarab@gmail.com>
2014-08-25 11:00:47 -0700

    modified:   .emacs.d/setup/setup-utilities.el

--- .emacs.d/setup/setup-utilities.el
+++ .emacs.d/setup/setup-utilities.el
@@ -142,5 +142,43 @@ buffer."
           ((string-match "[\]})>]" prev-char) (backward-sexp 1))
           (t (self-insert-command (or arg 1))))))
 
+(defvar *linum-mdown-line* nil)
+
+;; Multiple line select when clicking on fringe
+(defun line-at-click ()
+  (save-excursion
+    (let ((click-y (cdr (cdr (mouse-position))))
+          (line-move-visual-store line-move-visual))
+      (setq line-move-visual t)
+      (goto-char (window-start))
+      (next-line (1- click-y))
+      (setq line-move-visual line-move-visual-store)
+      ;; If you are using tabbar substitute the next line with
+      ;; (line-number-at-pos))))
+      (1+ (line-number-at-pos)))))
+
+(defun md-select-linum ()
+  (interactive)
+  (goto-line (line-at-click))
+  (set-mark (point))
+  (setq *linum-mdown-line*
+        (line-number-at-pos)))
+
+(defun mu-select-linum ()
+  (interactive)
+  (when *linum-mdown-line*
+    (let (mu-line)
+      ;; (goto-line (line-at-click))
+      (setq mu-line (line-at-click))
+      (goto-line (max *linum-mdown-line* mu-line))
+      (set-mark (line-end-position))
+      (goto-line (min *linum-mdown-line* mu-line))
+      (setq *linum-mdown*
+            nil))))
+
+(global-set-key (kbd "<left-margin> <down-mouse-1>") 'md-select-linum)
+(global-set-key (kbd "<left-margin> <mouse-1>") 'mu-select-linum)
+(global-set-key (kbd "<left-margin> <drag-mouse-1>") 'mu-select-linum)
+
 (provide 'setup-utilities)
 ;;; setup-utilities.el ends here
