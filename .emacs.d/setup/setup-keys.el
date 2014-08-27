3d8c282ff30af9b79c9bf1607656f39b331845b1
Abelardo Jara <abelardojarab@gmail.com>
2014-08-27 13:42:24 -0700

    modified:   .emacs.d/setup/setup-keys.el

--- .emacs.d/setup/setup-keys.el
+++ .emacs.d/setup/setup-keys.el
@@ -26,7 +26,7 @@
 
 ;; Ergoemacs
 (add-to-list 'load-path "~/.emacs.d/ergoemacs")
-(add-to-list 'load-path "~/.emacs.d/ergoemacs-keybindings")
+(add-to-list 'load-path "~/.emacs.d/ergoemacs-fixed-keybindings")
 (setq ergoemacs-ignore-prev-global nil) ;; Will not ignore any globally defined keybinding
 (setq ergoemacs-keyboard-layout "us")
 (require 'ergoemacs-mode)
@@ -49,51 +49,51 @@
 (define-key query-replace-map [?\C-m] 'act)
 
 ;; Zoom in/out like feature, without mouse wheel
-(global-set-key '[C-kp-add] 'text-scale-increase)
-(global-set-key '[C-kp-subtract] 'text-scale-decrease)
-(global-set-key '[C-+] 'text-scale-increase)
+(ergoemacs-fixed-key '[C-kp-add] 'text-scale-increase)
+(ergoemacs-fixed-key '[C-kp-subtract] 'text-scale-decrease)
+(ergoemacs-fixed-key '[C-+] 'text-scale-increase)
 
 ;; Commands to make my programming environment nice
-(global-set-key (kbd "RET") 'newline-and-indent)
-(global-set-key "\C-l" 'goto-line)
-(global-set-key (kbd "") 'other-window)
-(global-set-key [f5] 'compile)
-(global-set-key [f6] 'next-error)
-(global-set-key [f11] 'djcb-full-screen-toggle)
-(global-set-key [C-tab] 'comment-or-uncomment-region)
-(global-set-key [kp-prior] 'scroll-down)
-(global-set-key [prior] 'scroll-down)
-(global-set-key [kp-next] 'scroll-up)
-(global-set-key [next] 'scroll-up)
-(global-set-key "\M-g" 'goto-line)
-(global-set-key [home] 'beginning-of-line)
-(global-set-key [end] 'end-of-line)
-(global-set-key [delete] 'delete-char)
-(global-set-key [kp-delete] 'delete-char)
-(global-set-key [(meta delete)] '(lambda () (interactive) (backward-or-forward-kill-word -1)))
-(global-set-key [(alt delete)] '(lambda () (interactive) (backward-or-forward-kill-word -1)))
-(global-set-key [M-up] 'enlarge-window)
-(global-set-key [M-down] 'shrink-window)
-(global-set-key [(control o)] 'find-file)
-(global-set-key [(control n)] 'find-file-other-frame)
-(global-set-key [(control s)] 'save-buffer)
-(global-set-key [(meta s)] 'write-file)
-(global-set-key [(control q)] 'save-buffers-kill-emacs)
-(global-set-key [(meta q)] 'kill-this-buffer)
-(global-set-key [(control t)] 'ispell-buffer)
-(global-set-key [(control r)] 'replace-string)
-(global-set-key [(control z)] 'undo)
-(global-set-key "\C-a" 'mark-whole-buffer)
-(global-set-key (kbd "\C-c \C-c") 'kill-ring-save)
+(ergoemacs-fixed-key (kbd "RET") 'newline-and-indent)
+(ergoemacs-fixed-key "\C-l" 'goto-line)
+(ergoemacs-fixed-key (kbd "") 'other-window)
+(ergoemacs-fixed-key [f5] 'compile)
+(ergoemacs-fixed-key [f6] 'next-error)
+(ergoemacs-fixed-key [f11] 'djcb-full-screen-toggle)
+(ergoemacs-fixed-key [C-tab] 'comment-or-uncomment-region)
+(ergoemacs-fixed-key [kp-prior] 'scroll-down)
+(ergoemacs-fixed-key [prior] 'scroll-down)
+(ergoemacs-fixed-key [kp-next] 'scroll-up)
+(ergoemacs-fixed-key [next] 'scroll-up)
+(ergoemacs-fixed-key "\M-g" 'goto-line)
+(ergoemacs-fixed-key [home] 'beginning-of-line)
+(ergoemacs-fixed-key [end] 'end-of-line)
+(ergoemacs-fixed-key [delete] 'delete-char)
+(ergoemacs-fixed-key [kp-delete] 'delete-char)
+(ergoemacs-fixed-key [(meta delete)] '(lambda () (interactive) (backward-or-forward-kill-word -1)))
+(ergoemacs-fixed-key [(alt delete)] '(lambda () (interactive) (backward-or-forward-kill-word -1)))
+(ergoemacs-fixed-key [M-up] 'enlarge-window)
+(ergoemacs-fixed-key [M-down] 'shrink-window)
+(ergoemacs-fixed-key [(control o)] 'find-file)
+(ergoemacs-fixed-key [(control n)] 'find-file-other-frame)
+(ergoemacs-fixed-key [(control s)] 'save-buffer)
+(ergoemacs-fixed-key [(meta s)] 'write-file)
+(ergoemacs-fixed-key [(control q)] 'save-buffers-kill-emacs)
+(ergoemacs-fixed-key [(meta q)] 'kill-this-buffer)
+(ergoemacs-fixed-key [(control t)] 'ispell-buffer)
+(ergoemacs-fixed-key [(control r)] 'replace-string)
+(ergoemacs-fixed-key [(control z)] 'undo)
+(ergoemacs-fixed-key "\C-a" 'mark-whole-buffer)
+(ergoemacs-fixed-key (kbd "\C-c \C-c") 'kill-ring-save)
 
 ;; search forward with Ctrl-f
-(global-set-key [(control f)] 'isearch-forward)
+(ergoemacs-fixed-key [(control f)] 'isearch-forward)
 (define-key isearch-mode-map [(control f)] (lookup-key isearch-mode-map "\C-f"))
 (define-key minibuffer-local-isearch-map [(control f)]
   (lookup-key minibuffer-local-isearch-map "\C-s"))
 
 ;; search backward with Alt-f
-(global-set-key [(meta f)] 'isearch-backward)
+(ergoemacs-fixed-key [(meta f)] 'isearch-backward)
 (define-key isearch-mode-map [(meta f)] (lookup-key isearch-mode-map "\C-r"))
 (define-key minibuffer-local-isearch-map [(meta f)]
   (lookup-key minibuffer-local-isearch-map "\C-r"))
@@ -135,63 +135,63 @@
       (scroll-up increment)
       (smooth-scroll (- number-lines 1) increment))))
 
-(global-set-key [(mouse-5)] '(lambda () (interactive) (smooth-scroll 8 1)))
-(global-set-key [(mouse-4)] '(lambda () (interactive) (smooth-scroll 8 -1)))
+(ergoemacs-fixed-key [(mouse-5)] '(lambda () (interactive) (smooth-scroll 8 1)))
+(ergoemacs-fixed-key [(mouse-4)] '(lambda () (interactive) (smooth-scroll 8 -1)))
 
 ;; Zoom in/out like feature, with mouse wheel
 (global-unset-key (kbd "<C-wheel-up>")) ;; moved to <mode-line>
 (global-unset-key (kbd "<C-wheel-down>"))
-(global-set-key (kbd "<C-wheel-up>") 'text-scale-increase) ;; moved to <mode-line>
-(global-set-key (kbd "<C-wheel-down>") 'text-scale-decrease)
+(ergoemacs-fixed-key (kbd "<C-wheel-up>") 'text-scale-increase) ;; moved to <mode-line>
+(ergoemacs-fixed-key (kbd "<C-wheel-down>") 'text-scale-decrease)
 
 ;; Get the scroll wheel to work
-(global-set-key [(shift button5)] '(lambdas () (interactive) (scroll-up-line)))
-(global-set-key [(shift button4)] '(lambda () (interactive) (scroll-down-line)))
-(global-set-key [(control button5)] 'text-scale-decrease)
-(global-set-key [(control button4)] 'text-scale-increase)
+(ergoemacs-fixed-key [(shift button5)] '(lambdas () (interactive) (scroll-up-line)))
+(ergoemacs-fixed-key [(shift button4)] '(lambda () (interactive) (scroll-down-line)))
+(ergoemacs-fixed-key [(control button5)] 'text-scale-decrease)
+(ergoemacs-fixed-key [(control button4)] 'text-scale-increase)
 
-(global-set-key [(shift mouse-5)] '(lambda () (interactive) (scroll-up-line)))
-(global-set-key [(shift mouse-4)] '(lambda () (interactive) (scroll-down-line)))
-(global-set-key [(control mouse-5)] 'text-scale-decrease)
-(global-set-key [(control mouse-4)] 'text-scale-increase)
+(ergoemacs-fixed-key [(shift mouse-5)] '(lambda () (interactive) (scroll-up-line)))
+(ergoemacs-fixed-key [(shift mouse-4)] '(lambda () (interactive) (scroll-down-line)))
+(ergoemacs-fixed-key [(control mouse-5)] 'text-scale-decrease)
+(ergoemacs-fixed-key [(control mouse-4)] 'text-scale-increase)
 
 ;; higlight changes in documents
 (global-highlight-changes-mode t)
 (setq highlight-changes-visibility-initial-state nil)
 
 ;; toggle visibility
-(global-set-key (kbd "<f6>") 'highlight-changes-visible-mode) ;; changes
+(ergoemacs-fixed-key (kbd "<f6>") 'highlight-changes-visible-mode) ;; changes
 
 ;; remove the change-highlight in region
-(global-set-key (kbd "S-<f6>") 'highlight-changes-remove-highlight)
+(ergoemacs-fixed-key (kbd "S-<f6>") 'highlight-changes-remove-highlight)
 
 ;; If you're not already using it for something else...
-(global-set-key (kbd "<M-next>") 'highlight-changes-next-change)
-(global-set-key (kbd "<M-prior>")  'highlight-changes-previous-change)
+(ergoemacs-fixed-key (kbd "<M-next>") 'highlight-changes-next-change)
+(ergoemacs-fixed-key (kbd "<M-prior>")  'highlight-changes-previous-change)
 (set-face-foreground 'highlight-changes nil)
 (set-face-background 'highlight-changes "#882020")
 (set-face-foreground 'highlight-changes-delete nil)
 (set-face-background 'highlight-changes-delete "#916868")
 
 ;; toggle truncate lines
-(global-set-key (kbd "<f8>") 'toggle-truncate-lines)
+(ergoemacs-fixed-key (kbd "<f8>") 'toggle-truncate-lines)
 
 ;; Bookmark keys
-(global-set-key (kbd "<C-f2>") 'bm-toggle)
-(global-set-key (kbd "<f2>")   'bm-next)
-(global-set-key (kbd "<S-f2>") 'bm-previous)
+(ergoemacs-fixed-key (kbd "<C-f2>") 'bm-toggle)
+(ergoemacs-fixed-key (kbd "<f2>")   'bm-next)
+(ergoemacs-fixed-key (kbd "<S-f2>") 'bm-previous)
 
 ;; Refresh file on F9
 (defun refresh-file ()
   (interactive)
   (revert-buffer t t t))
-(global-set-key [f9] 'refresh-file)
+(ergoemacs-fixed-key [f9] 'refresh-file)
 
 ;; Code folding
 (defun toggle-selective-display ()
   (interactive)
   (set-selective-display (if selective-display nil 1)))
-(global-set-key [f1] 'toggle-selective-display)
+(ergoemacs-fixed-key [f1] 'toggle-selective-display)
 
 ;; Windows-like mouse/arrow movement & selection
 (transient-mark-mode 1)
@@ -206,8 +206,8 @@
 
 ;; Redo
 (require 'redo+)
-(global-set-key (kbd "C-S-z") 'redo) ; Mac style
-(global-set-key (kbd "C-y") 'redo) ; Microsoft Windows style
+(ergoemacs-fixed-key (kbd "C-S-z") 'redo) ; Mac style
+(ergoemacs-fixed-key (kbd "C-y") 'redo) ; Microsoft Windows style
 (setq undo-no-redo t)
 
 ;; Better undo
@@ -218,7 +218,7 @@
 (global-unset-key [(control mouse-3)])
 (require 'mouse3)
 (defalias 'mouse3-region-popup-menu 'mouse3-popup-menu)
-(global-set-key [(control mouse-3)] 'mouse3-popup-menu)
+(ergoemacs-fixed-key [(control mouse-3)] 'mouse3-popup-menu)
 
 ;; Trick emacs when opening a file through menu-find-file-existing
 (defadvice find-file-read-args (around find-file-read-args-always-use-dialog-box act)
