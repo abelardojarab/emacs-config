adfa74192075a80ec70f7bcdcb70f06075d1ce8c
Abelardo Jara <abelardojarab@gmail.com>
2014-09-09 10:31:00 -0700

    modified:   .emacs.d/setup/setup-tabbar.el

--- .emacs.d/setup/setup-tabbar.el
+++ .emacs.d/setup/setup-tabbar.el
@@ -32,22 +32,6 @@
 (global-set-key [C-next] 'tabbar-forward-tab)
 (global-set-key [M-prior] 'tabbar-backward-group)
 (global-set-key [M-next] 'tabbar-forward-group)
-(global-set-key [M-left] 'tabbar-backward-tab)
-(global-set-key [M-right] 'tabbar-forward-tab)
-
-;; Sort buffer names alphabetically
-(defun tabbar-add-tab (tabset object &optional append_ignored)
-  "Add to TABSET a tab with value OBJECT if there isn't one there yet.
- If the tab is added, it is added at the beginning of the tab list,
- unless the optional argument APPEND is non-nil, in which case it is
- added at the end."
-  (let ((tabs (tabbar-tabs tabset)))
-    (if (tabbar-get-tab object tabset)
-        tabs
-      (let ((tab (tabbar-make-tab object tabset)))
-        (tabbar-set-template tabset nil)
-        (set tabset (sort (cons tab tabs)
-                          (lambda (a b) (string< (buffer-name (car a)) (buffer-name (car b))))))))))
 
 ;; Tweaking the tabbar
 (defadvice tabbar-buffer-tab-label (after fixup_tab_label_space_and_flag activate)
@@ -130,10 +114,6 @@
               my-buffer-list)
        (bury-buffer)
        (nth n my-buffer-list)))))
-(global-set-key  [(control meta prior)] 'crs-bury-buffer)
-(global-set-key  [(control metal next)] (lambda ()
-                             (interactive)
-                             (crs-bury-buffer -1)))
 
 (provide 'setup-tabbar)
 ;;; setup-tabbar.el ends here
