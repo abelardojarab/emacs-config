715d3976da3f006300630f677ccb95665f146576
Abelardo Jara <abelardojarab@gmail.com>
2014-09-15 09:21:57 -0700

    modified:   .emacs.d/setup/setup-appearance.elc

--- .emacs.d/setup/setup-appearance.el
+++ .emacs.d/setup/setup-appearance.el
@@ -340,15 +340,5 @@
 
 (global-lawlist-scroll-bar-mode)
 
-;; horizontal scrolling
-(if (boundp 'truncate-lines)
-    (setq-default truncate-lines t) ; always truncate
-  (progn
-    (hscroll-global-mode t)
-    (setq hscroll-margin 1)
-    (setq auto-hscroll-mode 1)
-    (setq automatic-hscrolling t)
-    ))
-
 (provide 'setup-appearance)
 ;;; setup-appearance.el ends here
