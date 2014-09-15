899ea1332dbb255e3a1a4d9a4dc080658d2ce2d1
Abelardo Jara <abelardojarab@gmail.com>
2014-09-15 11:24:10 -0700

    modified:   .emacs.d/setup/setup-tabbar.el

--- .emacs.d/setup/setup-tabbar.el
+++ .emacs.d/setup/setup-tabbar.el
@@ -37,7 +37,7 @@
 (defadvice tabbar-buffer-tab-label (after fixup_tab_label_space_and_flag activate)
   (setq ad-return-value
         (if (and (buffer-modified-p (tabbar-tab-value tab))
-                 (buffer-file-name (tabbar-tab-value tab)))
+               (buffer-file-name (tabbar-tab-value tab)))
             (concat "+" (concat ad-return-value ""))
           (concat "" (concat ad-return-value "")))))
 
@@ -60,13 +60,52 @@
 (setq tabbar-separator '(0.5))
 
 ;; Define tabbar groups
-(setq tabbar-buffer-groups-function
-      (lambda ()
-        (list (cond
-               ((string-equal "*" (substring (buffer-name) 0 1)) "Emacs")
-               ((eq major-mode 'dired-mode) "Dired")
-               ((eq major-mode 'compilation-mode) "Compilation")
-               (t "User")))))
+(defun tabbar-buffer-groups-function ()
+  "Return the list of group names the current buffer belongs to.
+Return a list of one element based on major mode."
+  (list
+   (setq my-group-by-project nil)
+   (cond
+    ;; ((or (get-buffer-process (current-buffer))
+    ;;      ;; Check if the major mode derives from `comint-mode' or
+    ;;      ;; `compilation-mode'.
+    ;;      (tabbar-buffer-mode-derived-p
+    ;;       major-mode '(comint-mode compilation-mode)))
+    ;;  "Process"
+    ;;  )
+    ((member (buffer-name)
+             '("*scratch*" "*Messages*"))
+     "Common"
+     )
+    ((eq major-mode 'dired-mode)
+     "Dired"
+     )
+    ((memq major-mode
+           '(help-mode apropos-mode Info-mode Man-mode))
+     "Help"
+     )
+    ((memq major-mode
+           '(rmail-mode
+             rmail-edit-mode vm-summary-mode vm-mode mail-mode
+             mh-letter-mode mh-show-mode mh-folder-mode
+             gnus-summary-mode message-mode gnus-group-mode
+             gnus-article-mode score-mode gnus-browse-killed-mode))
+     "Mail"
+     )
+    (t
+     ;; Return `mode-name' if not blank, `major-mode' otherwise.
+     (let ((group
+            (if (and (stringp mode-name)
+                   ;; Take care of preserving the match-data because this
+                   ;; function is called when updating the header line.
+                   (save-match-data (string-match "[^ ]" mode-name)))
+                mode-name
+              (symbol-name major-mode))))
+       (if (projectile-project-p)
+           (if my-group-by-project
+               (projectile-project-name)
+             (format "%s:%s" (projectile-project-name) group))
+         group))))))
 
 ;; necessary support function for buffer burial
 (defun crs-delete-these (delete-these from-this-list)
