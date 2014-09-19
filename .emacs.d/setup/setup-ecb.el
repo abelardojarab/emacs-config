ace4d912644d7e5300a97bf25702ccbb7af1b0c7
Abelardo Jara <abelardojarab@gmail.com>
2014-09-19 09:09:52 -0700

    new file:   .emacs.d/elisp/projectile-speedbar.el

--- .emacs.d/setup/setup-ecb.el
+++ .emacs.d/setup/setup-ecb.el
@@ -72,32 +72,32 @@
 (defun ecb-enlarge-frame-width-before-show ()
   "Enlarge frame width before ecb shows layout."
   (if (and (ecb-windows-all-hidden)
-           (<= (+ (frame-pixel-width) (* (frame-char-width)
-                                         (+ ecb-windows-width 2)))
-               (display-pixel-width)))
+         (<= (+ (frame-pixel-width) (* (frame-char-width)
+                                      (+ ecb-windows-width 2)))
+            (display-pixel-width)))
       (set-frame-width (selected-frame) (+ (frame-width) (+ ecb-windows-width 2)))))
 (defun ecb-shrink-frame-width-before-hide ()
   "Shrink frame width before ecb hide layout."
   (if (and (not (ecb-windows-all-hidden))
 
-           (not (eq (frame-pixel-width)
-                    (display-pixel-width))))
+         (not (eq (frame-pixel-width)
+                (display-pixel-width))))
       (if (< (- (frame-width) (+ ecb-windows-width 2)) initial-frame-width)
           (set-frame-width (selected-frame) initial-frame-width)
         (set-frame-width (selected-frame) (- (frame-width) (+ ecb-windows-width 2))))))
 (defun ecb-enlarge-frame-width-before-activate ()
   "Enlarge frame width when ecb active and need it to."
   (let ((use-last-win-conf (and ecb-last-window-config-before-deactivation
-                                (equal ecb-split-edit-window-after-start
-                                       'before-deactivation)
-                                (not (ecb-window-configuration-invalidp
-                                      ecb-last-window-config-before-deactivation)))))
+                              (equal ecb-split-edit-window-after-start
+                                     'before-deactivation)
+                              (not (ecb-window-configuration-invalidp
+                                  ecb-last-window-config-before-deactivation)))))
     (unless (or (and use-last-win-conf
-                     (eq (nth 5 ecb-last-window-config-before-deactivation)
-                         ecb-windows-hidden-all-value))
-                (> (+ (frame-pixel-width) (* (frame-char-width)
-                                             (+ ecb-windows-width 2)))
-                   (display-pixel-width)))
+                  (eq (nth 5 ecb-last-window-config-before-deactivation)
+                      ecb-windows-hidden-all-value))
+               (> (+ (frame-pixel-width) (* (frame-char-width)
+                                            (+ ecb-windows-width 2)))
+                  (display-pixel-width)))
       (set-frame-width (selected-frame) (+ (frame-width) (+ ecb-windows-width 2))))))
 
 ;; reference path-to-ecb/ecb-layout-defs.el
@@ -170,19 +170,119 @@ little more place. "
   (select-window (next-window)))
 
 ;; Speedbar
+(require 'sr-speedbar)
+(setq speedbar-hide-button-brackets-flag t
+      speedbar-show-unknown-files t
+      speedbar-smart-directory-expand-flag t
+      speedbar-directory-button-trim-method 'trim
+      speedbar-use-images nil
+      speedbar-indentation-width 2
+      speedbar-use-imenu-flag t
+      speedbar-file-unshown-regexp "flycheck-.*"
+      sr-speedbar-width 40
+      sr-speedbar-width-x 40
+      sr-speedbar-auto-refresh t
+      sr-speedbar-skip-other-window-p t
+      sr-speedbar-right-side nil)
+
+;; Refresh the speedbar when relevant hooks are run.
+(defvar graphene-speedbar-refresh-hooks-added nil
+  "Whether hooks have been added to refresh speedbar.")
+(defvar graphene-speedbar-refresh-hooks nil
+  "Dummy variable.")
+
+(add-hook 'speedbar-mode-hook
+          (when (not graphene-speedbar-refresh-hooks-added)
+            (lambda ()
+              (mapc (lambda (hook)
+                      (add-hook hook 'speedbar-refresh))
+                    graphene-speedbar-refresh-hooks)
+              (setq graphene-speedbar-refresh-hooks t))))
+
+;; More familiar keymap settings.
+(add-hook 'speedbar-reconfigure-keymaps-hook
+          '(lambda ()
+             (define-key speedbar-mode-map [S-up] 'speedbar-up-directory)
+             (define-key speedbar-mode-map [right] 'speedbar-flush-expand-line)
+             (define-key speedbar-mode-map [left] 'speedbar-contract-line)))
+
+;; Highlight the current line
+(add-hook 'speedbar-mode-hook '(lambda () (hl-line-mode 1)))
+
+;; Pin and unpin the speedbar
+(defadvice speedbar-update-directory-contents
+  (around graphene-speedbar-pin-directory activate disable)
+  "Pin the speedbar to the directory set in graphene-speedbar-pinned-directory."
+  (let ((default-directory graphene-speedbar-pinned-directory))
+    ad-do-it))
+
+(defadvice speedbar-dir-follow
+  (around graphene-speedbar-prevent-follow activate disable)
+  "Prevent speedbar changing directory on button clicks."
+  (speedbar-toggle-line-expansion))
+
+(defadvice speedbar-directory-buttons-follow
+  (around graphene-speedbar-prevent-root-follow activate disable)
+  "Prevent speedbar changing root directory on button clicks.")
+
+(defvar graphene-speedbar-pin-advice
+  '((speedbar-update-directory-contents around graphene-speedbar-pin-directory)
+    (speedbar-dir-follow around graphene-speedbar-prevent-follow)
+    (speedbar-directory-buttons-follow around graphene-speedbar-prevent-root-follow))
+  "Advice to be enabled and disabled on graphene-[un]-pin-speedbar.")
+
+(defun graphene-speedbar-pin-advice-activate ()
+  "Activate the advice applied to speedbar functions in order to pin it to a directory."
+  (mapc 'ad-activate (mapcar 'car graphene-speedbar-pin-advice)))
+
+(defun graphene-pin-speedbar (directory)
+  "Prevent the speedbar from changing the displayed root directory."
+  (setq graphene-speedbar-pinned-directory directory)
+  (mapc (lambda (ls) (apply 'ad-enable-advice ls)) graphene-speedbar-pin-advice)
+  (graphene-speedbar-pin-advice-activate))
+
+(defun graphene-unpin-speedbar ()
+  "Allow the speedbar to change the displayed root directory."
+  (mapc (lambda (ls) (apply 'ad-disable-advice ls)) graphene-speedbar-pin-advice)
+  (graphene-speedbar-pin-advice-activate))
+
+;; Always use the last selected window for loading files from speedbar.
+(defvar last-selected-window
+  (if (not (eq (selected-window) sr-speedbar-window))
+      (selected-window)
+    (other-window)))
+
+(defadvice select-window (after remember-selected-window activate)
+  "Remember the last selected window."
+  (unless (or (eq (selected-window) sr-speedbar-window) (not (window-live-p (selected-window))))
+    (setq last-selected-window (selected-window))))
+
+(defun sr-speedbar-before-visiting-file-hook ()
+  "Function that hooks `speedbar-before-visiting-file-hook'."
+  (select-window last-selected-window))
+
+(defun sr-speedbar-before-visiting-tag-hook ()
+  "Function that hooks `speedbar-before-visiting-tag-hook'."
+  (select-window last-selected-window))
+
+(defun sr-speedbar-visiting-file-hook ()
+  "Function that hooks `speedbar-visiting-file-hook'."
+  (select-window last-selected-window))
+
+(defun sr-speedbar-visiting-tag-hook ()
+  "Function that hooks `speedbar-visiting-tag-hook'."
+  (select-window last-selected-window))
+
+;; Add Javascript
 (speedbar-add-supported-extension ".js")
 (add-to-list 'speedbar-fetch-etags-parse-list
              '("\\.js" . speedbar-parse-c-or-c++tag))
+(speedbar-add-supported-extension ".il")
+(speedbar-add-supported-extension ".ils")
 
-;; don't refresh on buffer changes
-(setq sr-speedbar-auto-refresh t)
-
-;; turn off the ugly icons
-(setq speedbar-use-images nil)
-
-;; nicer fonts for speedbar when in GUI
+;; Nicer fonts for speedbar when in GUI
 (when (and (window-system)
-           (find-font (font-spec :name "Consolas")))
+         (find-font (font-spec :name "Consolas")))
   ;; keep monospace buttons, but smaller height
   (set-face-attribute 'speedbar-button-face nil :height 100)
 
@@ -192,7 +292,10 @@ little more place. "
                       'speedbar-highlight-face))
     (if (eq system-type 'windows-nt)
         (set-face-attribute face nil :family "Consolas" :height 110))
-    (set-face-attribute face nil :family "Consolas" :height 110)))
+    (set-face-attribute face nil :family "Consolas" :height 135)))
+
+;; Projectile integration
+(require'projectile-speedbar)
 
 (provide 'setup-ecb)
 ;;; setup-ecb.el ends here
