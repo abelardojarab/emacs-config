3921941a668d35438adccc087804182cbae54ee3
Abelardo Jara <abelardojarab@gmail.com>
2014-09-23 11:55:35 -0700

    modified:   .emacs.d/setup/setup-org.el

--- .emacs.d/setup/setup-org.el
+++ .emacs.d/setup/setup-org.el
@@ -530,6 +530,7 @@ a link to this file."
 (add-to-list 'org-latex-packages-alist '("" "caption"))
 (add-to-list 'org-latex-packages-alist '("" "listings"))
 (add-to-list 'org-latex-packages-alist '("" "color"))
+(add-to-list 'org-latex-packages-alist '("" "mathptmx"))
 (add-to-list 'org-latex-classes
              '("article"
                "\\documentclass[10pt,article,oneside]{memoir}"
@@ -548,43 +549,6 @@ a link to this file."
                ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
                ("\\paragraph{%s}" . "\\paragraph*{%s}")
                ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
-(add-to-list 'org-latex-classes
-             ;; beamer class, for presentations
-             '("beamer"
-               "\\documentclass[11pt]{beamer}\n
-\\mode<{{{beamermode}}}>\n
-\\usetheme{{{{beamertheme}}}}\n
-\\usecolortheme{{{{beamercolortheme}}}}\n
-\\beamertemplateballitem\n
-\\setbeameroption{show notes}
-\\usepackage[utf8]{inputenc}\n
-\\usepackage[T1]{fontenc}\n
-\\usepackage{hyperref}\n
-\\usepackage{color}
-\\usepackage{listings}
-\\usepackage{geometry}
-\\usepackage{graphicx}
-\\usepackage{mathptmx}
-\\usepackage[section]{placeins}
-\\lstset{numbers=none,language=[ISO]C++,tabsize=4,
-frame=single,
-basicstyle=\\small,
-showspaces=false,showstringspaces=false,
-showtabs=false,
-keywordstyle=\\color{blue}\\bfseries,
-commentstyle=\\color{red},
-}\n
-
-\\usepackage{verbatim}\n
-\\institute{{{{beamerinstitute}}}}\n
-\\subject{{{{beamersubject}}}}\n"
-
-               ("\\section{%s}" . "\\section*{%s}")
-
-               ("\\begin{frame}[fragile]\\frametitle{%s}"
-                "\\end{frame}"
-                "\\begin{frame}[fragile]\\frametitle{%s}"
-                "\\end{frame}")))
 
 (add-to-list 'org-latex-classes
              '("xelatex"
