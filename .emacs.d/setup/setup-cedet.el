e92af96e861c6459438f4d0babfa4fd4492b5119
Abelardo Jara-Berrocal <abelardotomasjb@gmail.com>
2014-10-17 14:40:53 -0700

    modified:   setup-cedet.el

--- .emacs.d/setup/setup-cedet.el
+++ .emacs.d/setup/setup-cedet.el
@@ -248,9 +248,9 @@
 
 ;; Function arguments
 (add-to-list 'load-path "~/.emacs.d/functions-args")
-(load "~/.emacs.d/function-args/function-args.el")
+;; (load "~/.emacs.d/function-args/function-args.el")
 ;; (require 'function-args)
-(fa-config-default)
+;; (fa-config-default)
 
 ;; Formatting
 (require 'zjl-hl)
--- .emacs.d/setup/setup-js2-mode.el
+++ .emacs.d/setup/setup-js2-mode.el
@@ -11,13 +11,12 @@
 
 (require 'js2-mode)
 (require 'ac-js2)
-(require 'node-ac-mode)
 
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
 (add-hook 'js2-mode-hook (lambda () (flycheck-mode 1)))
 (add-hook 'js2-mode-hook 'skewer-mode)
-;; (add-hook 'js2-mode-hook 'ac-js2-mode)
+(add-hook 'js2-mode-hook 'ac-js2-mode)
 (setq ac-js2-evaluate-calls t)
 
 (setq-default js2-allow-rhino-new-expr-initializer nil)
