273420d3056271399decf6847e0b8d3eb7c4e806
Abelardo Jara-Berrocal <abelardotomasjb@gmail.com>
2014-10-17 14:26:39 -0700

    modified:   setup-js2-mode.el

--- .emacs.d/setup/setup-js2-mode.el
+++ .emacs.d/setup/setup-js2-mode.el
@@ -15,9 +15,9 @@
 
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
-(add-hook 'js2-mode-hook 'ac-js2-mode)
 (add-hook 'js2-mode-hook (lambda () (flycheck-mode 1)))
 (add-hook 'js2-mode-hook 'skewer-mode)
+;; (add-hook 'js2-mode-hook 'ac-js2-mode)
 (setq ac-js2-evaluate-calls t)
 
 (setq-default js2-allow-rhino-new-expr-initializer nil)
@@ -221,31 +221,5 @@
 
 (eval-after-load 'css-mode
   '(define-key css-mode-map (kbd "C-c b") 'web-beautify-css))
-
-(eval-after-load 'js2-mode
-  '(add-hook 'js2-mode-hook
-             (lambda ()
-               (add-hook 'before-save-hook 'web-beautify-js-buffer t t))))
-
-;; Or if you're using 'js-mode' (a.k.a 'javascript-mode')
-(eval-after-load 'js
-  '(add-hook 'js-mode-hook
-             (lambda ()
-               (add-hook 'before-save-hook 'web-beautify-js-buffer t t))))
-
-(eval-after-load 'json-mode
-  '(add-hook 'json-mode-hook
-             (lambda ()
-               (add-hook 'before-save-hook 'web-beautify-js-buffer t t))))
-
-(eval-after-load 'sgml-mode
-  '(add-hook 'html-mode-hook
-             (lambda ()
-               (add-hook 'before-save-hook 'web-beautify-html-buffer t t))))
-
-(eval-after-load 'css-mode
-  '(add-hook 'css-mode-hook
-             (lambda ()
-               (add-hook 'before-save-hook 'web-beautify-css-buffer t t))))
 
 (provide 'setup-js2-mode)
