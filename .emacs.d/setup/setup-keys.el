8504979391f5267648a2d5c1134faa959b909bc0
Abelardo Jara-Berrocal <abelardotomasjb@gmail.com>
2014-10-29 09:29:08 -0700

    new file:   "../cedet/tests/integ_src/globalref\200inc/Manager.hh"

--- .emacs.d/setup/setup-keys.el
+++ .emacs.d/setup/setup-keys.el
@@ -86,26 +86,17 @@
 (global-set-key "\C-a" 'mark-whole-buffer)
 (global-set-key (kbd "\C-c \C-c") 'kill-ring-save)
 
-(if (executable-find "python")
-    (progn
-      (global-set-key [(meta f)] 'vr/isearch-backward) ;; C-M-r
-      (global-set-key [(control f)] 'vr/isearch-forward) ;; C-M-s
-      ) ;; progn
+;; search forward with Ctrl-f
+(global-set-key [(control f)] 'isearch-forward)
+(define-key isearch-mode-map [(control f)] (lookup-key isearch-mode-map "\C-f"))
+(define-key minibuffer-local-isearch-map [(control f)]
+  (lookup-key minibuffer-local-isearch-map "\C-s"))
 
-    (progn
-      ;; search forward with Ctrl-f
-      (global-set-key [(control f)] 'isearch-forward)
-      (define-key isearch-mode-map [(control f)] (lookup-key isearch-mode-map "\C-f"))
-      (define-key minibuffer-local-isearch-map [(control f)]
-        (lookup-key minibuffer-local-isearch-map "\C-s"))
-
-      ;; search backward with Alt-f
-      (global-set-key [(meta f)] 'isearch-backward)
-      (define-key isearch-mode-map [(meta f)] (lookup-key isearch-mode-map "\C-r"))
-      (define-key minibuffer-local-isearch-map [(meta f)]
-        (lookup-key minibuffer-local-isearch-map "\C-r"))
-      ) ;; progn
-    ) ;; if
+;; search backward with Alt-f
+(global-set-key [(meta f)] 'isearch-backward)
+(define-key isearch-mode-map [(meta f)] (lookup-key isearch-mode-map "\C-r"))
+(define-key minibuffer-local-isearch-map [(meta f)]
+  (lookup-key minibuffer-local-isearch-map "\C-r"))
 
 ;; Cancel minibuffer operation if you click outside
 (defun stop-using-minibuffer ()
