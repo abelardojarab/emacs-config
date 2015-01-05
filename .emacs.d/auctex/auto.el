fc7b229908d0bc18a1de776df327aaa323e5f382
Abelardo Jara <abelardojarab@gmail.com>
2015-01-05 14:34:17 -0800

    new file:   auctex/.cvsignore

---
+++ .emacs.d/auctex/auto.el
@@ -0,0 +1,5 @@
+
+(defvar preview-lispdir (expand-file-name "auctex" (file-name-directory load-file-name)))
+(add-to-list 'load-path preview-lispdir)
+(defvar preview-datadir (expand-file-name "auctex" (file-name-directory load-file-name)))
+
--- .emacs.d/auctex/site-lisp/site-start.el
+++
@@ -1,5 +0,0 @@
-;; Load files in `site-start.d' directory.
-(dolist (file (directory-files
-	       (concat (file-name-directory load-file-name) "site-start.d")
-	       t "\\.el\\'"))
-  (load file nil t t))
