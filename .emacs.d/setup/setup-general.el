24ff93ef76ace4feb7c29163b2bf2024ad29b83e
Abelardo Jara <abelardojarab@gmail.com>
2015-08-26 10:59:30 -0700

    modified:   .emacs.d/setup/setup-general.el

--- .emacs.d/setup/setup-general.el
+++ .emacs.d/setup/setup-general.el
@@ -271,19 +271,6 @@
       (setq sanityinc/longlines-mode-suppressed nil)
       (longlines-mode 1))))
 
-;; Fix fci related issues
-(defvar sanityinc/fci-mode-suppressed nil)
-(defadvice popup-create (before suppress-fci-mode activate)
-  "Suspend fci-mode while popups are visible"
-  (set (make-local-variable 'sanityinc/fci-mode-suppressed) fci-mode)
-  (when fci-mode
-    (turn-off-fci-mode)))
-(defadvice popup-delete (after restore-fci-mode activate)
-  "Restore fci-mode when all popups have closed"
-  (when (and (not popup-instances) sanityinc/fci-mode-suppressed)
-    (setq sanityinc/fci-mode-suppressed nil)
-    (turn-on-fci-mode)))
-
 ;; Avoid popup windows too
 (add-to-list 'load-path "~/.emacs.d/popwin")
 (require 'popwin)
