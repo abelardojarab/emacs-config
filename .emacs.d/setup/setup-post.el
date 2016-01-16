9996a71d2d4d755276e06bed590971ea8f75294f
Abelardo Jara-Berrocal <abelardojarab@gmail.com>
2016-01-15 16:01:22 -0800

    modified:   setup/setup-post.el

--- .emacs.d/setup/setup-post.el
+++ .emacs.d/setup/setup-post.el
@@ -76,6 +76,7 @@
 (add-to-list 'load-path "~/.emacs.d/ergoemacs-mode")
 (setq ergoemacs-ignore-prev-global nil)
 (setq ergoemacs-component-struct--apply-ensure-p nil)
+(setq ergoemacs-theme--custom-documentation nil)
 (setq ergoemacs-keyboard-layout "us")
 (setq ergoemacs-theme "lvl0")
 (require 'ergoemacs-mode)
