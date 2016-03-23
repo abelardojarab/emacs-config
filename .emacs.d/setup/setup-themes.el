37c133586a1f6c489ca236a810bb346c9714f507
Abelardo Jara <abelardojarab@gmail.com>
2016-03-23 11:16:06 -0700

    modified:   .emacs.d/setup/setup-themes.el

--- .emacs.d/setup/setup-themes.el
+++ .emacs.d/setup/setup-themes.el
@@ -89,7 +89,7 @@
 ;; Different possible themes
 ;; (load-theme 'atom-dark t)
 ;; (load-theme 'zenburn t)
-;; (load-theme 'leuven t)
+(load-theme 'leuven t)
 ;; (load-theme 'zerodark t)
 ;; (load-theme 'material t)
 ;; (load-theme 'FlatUI t)
@@ -97,7 +97,7 @@
 ;; (load-theme 'twilight-brigh)
 ;; (load-theme 'plan9)
 ;; (load-theme 'minimal-light) ;; best ink theme
-(load-theme 'monokai t) ;; best dark color scheme
+;; (load-theme 'monokai t) ;; best dark color scheme
 ;; (load-theme 'eink)
 ;; (load-theme 'monokai-extended t)
 ;; (load-theme 'pastelmac t)
