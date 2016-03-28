95afe6596d649f397c85dfdea041b80e1c89cb57
Abelardo Jara <abelardojarab@gmail.com>
2016-03-28 11:00:56 -0700

    modified:   .emacs.d/setup/setup-fonts.el

--- .emacs.d/setup/setup-fonts.el
+++ .emacs.d/setup/setup-fonts.el
@@ -88,7 +88,7 @@
              (if (> (x-display-pixel-width) 1800)
                  (progn ;; HD monitor in Windows
                    (setq main-programming-font (concat main-programming-font "-12"))
-                   (setq main-writing-font (concat main-writing-font "-13")))
+                   (setq main-writing-font (concat main-writing-font "-15")))
                (progn
                  (setq main-programming-font (concat main-programming-font "-12"))
                  (setq main-writing-font (concat main-writing-font "-13")))))
