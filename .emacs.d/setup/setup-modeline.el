3abe473af542e44689b065e22dcc337e0664a65e
Abelardo Jara-Berrocal <abelardotomasjb@gmail.com>
2016-03-31 08:26:06 -0700

    Cleaning modeline.

--- .emacs.d/setup/setup-modeline.el
+++ .emacs.d/setup/setup-modeline.el
@@ -36,6 +36,42 @@
   :load-path (lambda () (expand-file-name "powerline/" user-emacs-directory))
   :init (setq powerline-default-separator 'wave)
   :config (progn
+            ;; Cleaning the mode line
+            ;; https://www.masteringemacs.org/article/hiding-replacing-modeline-strings
+            (defvar mode-line-cleaner-alist
+              `((auto-complete-mode . " α")
+                (yas/minor-mode . " υ")
+                (paredit-mode . " π")
+                (eldoc-mode . "")
+                (abbrev-mode . "")
+                (git-gutter+-mode . " ┢")
+                (smartparens-mode . " π")
+
+                ;; Major modes
+                (hi-lock-mode . "")
+                (python-mode . "Py")
+                (emacs-lisp-mode . "λ")
+                (nxhtml-mode . "nx"))
+
+              "Alist for `clean-mode-line'.
+When you add a new element to the alist, keep in mind that you
+must pass the correct minor/major mode symbol and a string you
+want to use in the modeline *in lieu of* the original.")
+
+            (defun clean-mode-line ()
+              (interactive)
+              (loop for cleaner in mode-line-cleaner-alist
+                    do (let* ((mode (car cleaner))
+                              (mode-str (cdr cleaner))
+                              (old-mode-str (cdr (assq mode minor-mode-alist))))
+                         (when old-mode-str
+                           (setcar old-mode-str mode-str))
+                         ;; major mode
+                         (when (eq mode major-mode)
+                           (setq mode-name mode-str)))))
+            (add-hook 'after-change-major-mode-hook 'clean-mode-line)
+
+            ;; Powerline setup
             (add-hook 'desktop-after-read-hook 'powerline-reset)
             (defface modes-ml-face '((t (:background "#002b36" :inherit mode-line)))
               "Powerline face for modes section of the mode-line"
