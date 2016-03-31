e5dd132bdd8333931224639f27cbda0489d6701f
Abelardo Jara <abelardojarab@gmail.com>
2016-03-31 10:15:15 -0700

    modified:   .emacs.d/setup/setup-ido.el

--- .emacs.d/setup/setup-ido.el
+++ .emacs.d/setup/setup-ido.el
@@ -29,7 +29,6 @@
   :config (progn
             (ido-mode 'both)
             (ido-everywhere 1)
-
             (setq
              ido-max-dir-file-cache 0
              ido-show-dot-for-dired t
@@ -52,24 +51,7 @@
              ido-confirm-unique-completion t)
 
             ;; when using ido, the confirmation is rather annoying...
-            (setq confirm-nonexistent-file-or-buffer nil)
-
-            (add-hook 'ido-make-file-list-hook 'ido-sort-mtime)
-            (add-hook 'ido-make-dir-list-hook 'ido-sort-mtime)
-            (defun ido-sort-mtime ()
-              (setq ido-temp-list
-                    (sort ido-temp-list
-                          (lambda (a b)
-                            (let ((ta (nth 5 (file-attributes (concat ido-current-directory a))))
-                                  (tb (nth 5 (file-attributes (concat ido-current-directory b)))))
-                              (if (= (nth 0 ta) (nth 0 tb))
-                                  (> (nth 1 ta) (nth 1 tb))
-                                (> (nth 0 ta) (nth 0 tb)))))))
-              (ido-to-end  ;; move . files to end (again)
-               (delq nil (mapcar
-                          (lambda (x) (if (string-equal (substring x 0 1) ".") x))
-                          ido-temp-list))))
-            ))
+            (setq confirm-nonexistent-file-or-buffer nil)))
 
 (provide 'setup-ido)
 ;;; setup-ido.el ends here
