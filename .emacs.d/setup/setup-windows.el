0f49d2f6dab8d78ff4896ba8f3659a299529829b
Abelardo Jara-Berrocal <abelardotomasjb@gmail.com>
2016-04-16 18:23:14 -0700

    new file:   .emacs.d/setup/setup-windows.el

---
+++ .emacs.d/setup/setup-windows.el
@@ -0,0 +1,51 @@
+;;; setup-windows.el ---                             -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2016  Abelardo Jara
+
+;; Author: Abelardo Jara <abelardojara@Abelardos-MacBook-Pro.local>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+;; Window purpose
+(use-package window-purpose
+  :load-path (lambda () (expand-file-name "window-purpose/" user-emacs-directory))
+  :init (progn
+          ;; overriding `purpose-mode-map' with empty keymap, so it doesn't conflict
+          ;; with original `C-x C-f', `C-x b', etc. and `semantic' key bindings. must
+          ;; be done before `window-purpose' is loaded
+          (setq purpose-mode-map (make-sparse-keymap)))
+  :config (progn
+            (require 'window-purpose-x)
+            (setq purpose-preferred-prompt 'helm)
+            (defalias 'window-purpose/helm-mini-ignore-purpose
+              (without-purpose-command #'helm-mini)
+              "Same as `helm-mini', but disable window-purpose while this command executes.")
+
+            (purpose-mode)
+            (purpose-x-popwin-setup)
+
+            ;; when killing a purpose-dedicated buffer that is displayed in a window,
+            ;; ensure that the buffer is replaced by a buffer with the same purpose
+            ;; (or the window deleted, if no such buffer)
+            (purpose-x-kill-setup)))
+
+(provide 'setup-windows)
+;;; setup-windows.el ends here
