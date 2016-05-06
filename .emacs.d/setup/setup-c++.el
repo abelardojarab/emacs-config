af0936de3fbfe4ecb73d0544af4a622de9181cf2
Abelardo Jara-Berrocal <abelardojarab@gmail.com>
2016-05-06 03:50:21 -0700

    modified:   .emacs.d/setup/setup-c++.el

--- .emacs.d/setup/setup-c++.el
+++ .emacs.d/setup/setup-c++.el
@@ -41,7 +41,6 @@
               (setq c-default-style "bsd")
               (setq c-basic-offset 4)
               (setq c-indent-level 4)
-              (setq c-tab-always-indent t)
               (setq-default tab-width 4)
               (setq-default tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 84 88 92 96 100))
               (setq comment-multi-line t))
