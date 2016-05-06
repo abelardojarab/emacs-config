6b58dd29907a77f9598c354964ae0e04f1bef490
Abelardo Jara-Berrocal <abelardojarab@gmail.com>
2016-05-06 02:21:39 -0700

    modified:   .emacs.d/setup/setup-flycheck.el

--- .emacs.d/setup/setup-flycheck.el
+++ .emacs.d/setup/setup-flycheck.el
@@ -54,29 +54,6 @@
             ;; Highlight whole line with error
             (setq flycheck-highlighting-mode 'lines)
 
-            ;; Define a poor c/c++ checker (it fails when errors affect other files,
-            ;; not the one being being checked actually)
-            (defmacro flycheck-define-clike-checker (name command modes)
-              `(flycheck-define-checker ,(intern (format "%s" name))
-                 ,(format "A %s checker using %s" name (car command))
-                 :command (,@command source-inplace)
-                 :error-patterns
-                 ((warning line-start (file-name) ":" line ":" column
-                           ": warning: " (message) line-end)
-                  (error line-start (file-name) ":" line ":" column
-                         ": error: " (message) line-end))
-                 :modes ',modes))
-            (when (executable-find "gcc")
-              (flycheck-define-clike-checker c-gcc
-                                             ("gcc" "-fsyntax-only" "-Wall" "-Wextra")
-                                             c-mode)
-              (add-to-list 'flycheck-checkers 'c-gcc))
-            (when (executable-find "g++")
-              (flycheck-define-clike-checker c++-g++
-                                             ("g++" "-fsyntax-only" "-Wall" "-Wextra" "-std=c++11")
-                                             c++-mode)
-              (add-to-list 'flycheck-checkers 'c++-g++))
-
             ;; proselint support
             (when (executable-find "proselint")
               (flycheck-define-checker proselint
@@ -86,7 +63,7 @@
                 ((warning line-start (file-name) ":" line ":" column ": "
                           (id (one-or-more (not (any " "))))
                           (message) line-end))
-                :modes (text-mode markdown-mode gfm-mode org-mode))
+                :modes (text-mode markdown-mode org-mode))
               (add-to-list 'flycheck-checkers 'proselint))
 
             ;; Display error messages on one line in minibuffer and by new lines
