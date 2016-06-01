f27fea4d66d73b67087acc99ea227ff23217dbec
Abelardo Jara <abelardojarab@gmail.com>
2016-05-31 17:24:35 -0700

    modified:   .emacs.d/setup/setup-highlight.el

--- .emacs.d/setup/setup-highlight.el
+++ .emacs.d/setup/setup-highlight.el
@@ -59,28 +59,29 @@
   :commands highlight-symbol-mode
   :load-path (lambda () (expand-file-name "highlight-symbol/" user-emacs-directory))
   :diminish highlight-symbol-mode
-  :config (progn (mapc (lambda (mode)
-                         (add-hook mode 'highlight-symbol-mode))
-                       '(prog-mode-hook))
+  :config (progn
+            ;; (mapc (lambda (mode)
+            ;;         (add-hook mode 'highlight-symbol-mode))
+            ;;       '(prog-mode-hook))
 
-                 ;; http://emacs.stackexchange.com/questions/931
-                 (defun highlight-symbol-mode-post-command ()
-                   "After a command, change the temporary highlighting.
+            ;; http://emacs.stackexchange.com/questions/931
+            (defun highlight-symbol-mode-post-command ()
+              "After a command, change the temporary highlighting.
 Remove the temporary symbol highlighting and, unless a timeout is specified,
 create the new one."
-                   (if (eq this-command 'highlight-symbol-jump)
-                       (when highlight-symbol-on-navigation-p
-                         (highlight-symbol-temp-highlight))
-                     (highlight-symbol-update-timer highlight-symbol-idle-delay)))
+              (if (eq this-command 'highlight-symbol-jump)
+                  (when highlight-symbol-on-navigation-p
+                    (highlight-symbol-temp-highlight))
+                (highlight-symbol-update-timer highlight-symbol-idle-delay)))
 
-                 (defun highlight-symbol-update-timer (value)
-                   (when highlight-symbol-timer
-                     (cancel-timer highlight-symbol-timer))
-                   (setq highlight-symbol-timer
-                         (run-with-timer value nil 'highlight-symbol-temp-highlight)))
+            (defun highlight-symbol-update-timer (value)
+              (when highlight-symbol-timer
+                (cancel-timer highlight-symbol-timer))
+              (setq highlight-symbol-timer
+                    (run-with-timer value nil 'highlight-symbol-temp-highlight)))
 
-                 (setq highlight-symbol-idle-delay .1)
-                 (setq highlight-symbol-on-navigation-p t)))
+            (setq highlight-symbol-idle-delay .1)
+            (setq highlight-symbol-on-navigation-p t)))
 
 ;; Highlight blocks
 (use-package highlight-blocks
