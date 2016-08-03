9d3da6e210bc85d5612d79ea2ba392c3c2cc7aac
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2016-08-03 14:26:53 -0400

    modified:   .emacs.d/setup/setup-versioning.el

--- .emacs.d/setup/setup-versioning.el
+++ .emacs.d/setup/setup-versioning.el
@@ -67,18 +67,73 @@
   :bind (:map ctl-x-map
               ("v" . magit-status))
   :load-path (lambda () (expand-file-name "magit/lisp" user-emacs-directory))
-  :init (eval-after-load 'info
-          '(progn (info-initialize)
-                  (add-to-list 'Info-directory-list (expand-file-name "magit/Documentation" user-emacs-directory))))
+  :init (progn
+          (eval-after-load 'info
+            '(progn (info-initialize)
+                    (add-to-list 'Info-directory-list (expand-file-name "magit/Documentation" user-emacs-directory))))
+
+          ;; we no longer need vc-git
+          (delete 'Git vc-handled-backends)
+          ;; make magit status go full-screen but remember previous window
+          ;; settings
+          ;; from: http://whattheemacsd.com/setup-magit.el-01.html
+          (defadvice magit-status (around magit-fullscreen activate)
+            (window-configuration-to-register :magit-fullscreen)
+            ad-do-it
+            (delete-other-windows))
+
+          ;; Close popup when commiting - this stops the commit window
+          ;; hanging around
+          ;; From: http://git.io/rPBE0Q
+          (defadvice git-commit-commit (after delete-window activate)
+            (delete-window))
+
+          (defadvice git-commit-abort (after delete-window activate)
+            (delete-window))
+
+          ;; these two force a new line to be inserted into a commit window,
+          ;; which stops the invalid style showing up.
+          ;; From: http://git.io/rPBE0Q
+          (defun magit-commit-mode-init ()
+            (when (looking-at "\n")
+              (open-line 1)))
+
+          (add-hook 'git-commit-mode-hook 'magit-commit-mode-init))
   :config (progn
-            (setq magit-commit-signoff nil
-                  magit-auto-revert-mode nil
-                  magit-process-popup-time 5
-                  magit-save-some-buffers nil
-                  magit-set-upstream-on-push t
-                  magit-diff-refine-hunk t
-                  magit-auto-revert-mode-lighter nil
-                  magit-completing-read-function 'magit-ido-completing-read)
+            (setq ;; don't put "origin-" in front of new branch names by default
+             magit-default-tracking-name-function 'magit-default-tracking-name-branch-only
+             ;; open magit status in same window as current buffer
+             magit-status-buffer-switch-function 'switch-to-buffer
+             ;; pop the process buffer if we're taking a while to complete
+             magit-process-popup-time 10
+             ;; ask me to save buffers
+             magit-save-some-buffers t
+             ;; ask me if I want a tracking upstream
+             magit-set-upstream-on-push 'askifnotset
+             ;; highlight word/letter changes in hunk diffs
+             magit-diff-refine-hunk t
+             ;; use ido to look for branches
+             magit-completing-read-function 'magit-ido-completing-read
+             ;; ask me if I want to include a revision when rewriting
+             magit-rewrite-inclusive 'ask)
+
+            ;; restore previously hidden windows
+            (defadvice magit-quit-window (around magit-restore-screen activate)
+              (let ((current-mode major-mode))
+                ad-do-it
+                ;; we only want to jump to register when the last seen buffer
+                ;; was a magit-status buffer.
+                (when (eq 'magit-status-mode current-mode)
+                  (jump-to-register :magit-fullscreen))))
+
+            (defun magit-maybe-commit (&optional show-options)
+              "Runs magit-commit unless prefix is passed"
+              (interactive "P")
+              (if show-options
+                  (magit-key-mode-popup-committing)
+                (magit-commit)))
+
+            (define-key magit-mode-map "c" 'magit-maybe-commit)
 
             ;; Disable saving place on git commit logs
             (add-hook 'git-commit-mode-hook (toggle-save-place 0))))
