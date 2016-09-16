5c614fd42ceb1603711dc56f9eed5405af4427c2
Lab <lab@atp-harp1.jf.intel.com>
2016-09-16 13:48:21 -0700

    modified:   setup/setup-fonts.el

--- .emacs.d/setup/setup-fonts.el
+++ .emacs.d/setup/setup-fonts.el
@@ -75,7 +75,7 @@
           (t ;; Linux
            (if (> (x-display-pixel-width) 2000)
                (progn ;; Ultra-HD monitor in Linux
-                 (setq main-programming-font (concat main-programming-font "-13"))
+                 (setq main-programming-font (concat main-programming-font "-14"))
                  (setq main-writing-font (concat main-writing-font "-15")))
              (if (> (x-display-pixel-width) 1800)
                  (progn ;; HD monitor in Linux
