6f891f22a6fc7539045505515e8e5786c2e1b5ec
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2016-11-09 13:20:16 -0800

    Fix the magit regex.

--- .emacs.d/setup/setup-flycheck.el
+++ .emacs.d/setup/setup-flycheck.el
@@ -42,9 +42,6 @@
                   '(prog-mode-hook
                     ess-mode-hook))
 
-            ;; Hide flycheck indicator
-            (diminish 'flycheck-mode)
-
             ;; disable flycheck during idle time, if enabled
             (delete 'idle-change flycheck-check-syntax-automatically)
 
