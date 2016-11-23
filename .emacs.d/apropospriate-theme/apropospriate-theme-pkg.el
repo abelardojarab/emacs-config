a5449870d8442c556b5de4c81e24054ae364dc67
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2016-11-22 22:05:27 -0800

    new file:   .emacs.d/apropospriate-theme/README.md

---
+++ .emacs.d/apropospriate-theme/apropospriate-theme-pkg.el
@@ -0,0 +1,5 @@
+(define-package
+  "apropospriate-theme"
+  "0.1.0"
+  "A colorful, low-contrast, light & dark theme set for Emacs with a fun name."
+  'nil :url "https://github.com/waymondo/apropospriate-theme" :keywords '("color" "theme"))
