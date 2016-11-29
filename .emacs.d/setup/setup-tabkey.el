566d26eac5d5d863bb85017df75d54c300731607
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2016-11-28 16:08:04 -0800

    new file:   .emacs.d/company-auctex/README.md

---
+++ .emacs.d/setup/setup-tabkey.el
@@ -0,0 +1,46 @@
+;;; setup-tabkey.el ---                              -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2016  Abelardo Jara-Berrocal
+
+;; Author: Abelardo Jara-Berrocal <abelardojara@ubuntu-MacBookPro>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+;; Smart tab
+(use-package smart-tab
+  :diminish smart-tab-mode
+  :load-path (lambda () (expand-file-name "smart-tab/" user-emacs-directory))
+  :config (progn
+            (cons 'yas-hippie-try-expand 'hippie-expand-try-functions-list)
+            (setq smart-tab-using-hippie-expand t
+                  smart-tab-completion-functions-alist (quote
+                                                        ((emacs-lisp-mode . company-complete)
+                                                         (c-mode . company-complete)
+                                                         (c++-mode . company-complete)
+                                                         (j2-mode . company-mode)
+                                                         (python-mode . company-complete)
+                                                         (markdown-mode . company-complete)
+                                                         (text-mode . company-complete)
+                                                         (org-mode . company-complete))))
+            (global-smart-tab-mode 1)))
+
+(provide 'setup-tabkey)
+;;; setup-tabkey.el ends here
