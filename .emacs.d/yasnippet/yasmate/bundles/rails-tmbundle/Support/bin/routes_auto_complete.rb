4d5441a18a73f8e853acc774f4920f122f213d56
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2016-12-26 12:03:53 -0800

    new file:   yasnippet/snippets/.nosearch

---
+++ .emacs.d/yasnippet/yasmate/bundles/rails-tmbundle/Support/bin/routes_auto_complete.rb
@@ -0,0 +1,10 @@
+#!/System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/bin/ruby
+
+require File.join(ENV['TM_SUPPORT_PATH'], 'lib', 'ui.rb')
+require File.join(ENV['TM_PROJECT_DIRECTORY'], 'config', 'environment')
+
+routes = ActionController::Routing::Routes.named_routes.routes.keys.map do |route|
+  %w(_path _url).map { |extension| route.to_s + extension } if route != :rails_info_properties
+end
+
+TextMate::UI.complete(routes.flatten.compact.sort, :extra_chars => "_")
