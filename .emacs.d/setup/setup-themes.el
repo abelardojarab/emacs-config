8eb04e200cc15dfba74f3e70cff923913db687bc
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2017-01-08 15:10:12 -0800

    Change default theme to zenburn.

--- .emacs.d/setup/setup-themes.el
+++ .emacs.d/setup/setup-themes.el
@@ -88,7 +88,7 @@
 ;; Choose different themes depending if we are using GUI or not
 ;; Console colors are enabled if "export TERM=xterm-256color" is added into .bashrc
 (if (display-graphic-p)
-    (load-theme 'leuven t)
+    (load-theme 'zenburn t)
   (load-theme 'monokai t))
 
 (provide 'setup-themes)
