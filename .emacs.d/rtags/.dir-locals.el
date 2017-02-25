95c8ac391da7297f1ea10cb1a6f4d152ea08ed1a
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2017-02-25 13:41:47 -0800

    Remove .dir-locals.el

--- .emacs.d/rtags/.dir-locals.el
+++
@@ -1,9 +0,0 @@
-;;; Directory Local Variables
-;;; See Info node `(emacs) Directory Variables' for more information.
-
-((c++-mode . ((c-basic-offset . 4)
-              (indent-tabs-mode . nil))))
-((cmake-mode . ((cmake-tab-width . 4)
-                (indent-tabs-mode . nil))))
-((nil . ((indent-tabs-mode . nil))))
-
