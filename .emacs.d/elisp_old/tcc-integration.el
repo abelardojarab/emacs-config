9f6648a4454b8bfee567f47a3329801c3060ffd9
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2017-04-19 13:37:27 -0700

    Fix issue with stacktrace not appearing

--- .emacs.d/elisp_old/tcc-integration.el
+++
@@ -1,9 +0,0 @@
-(defun start-skill ()
-  "Starts a local SKILL 'interpretter' that talks to skill over
-   a socket. Note that this socket has to exist a priori. This 
-   socket is automatically created on cadence startup so long as
-   the TCC_START_SERVER  environment variable is set (this is done
-   in $CDS_SITE/.cdsinit.testchip"
-  (interactive)
-  ;;point ilisp to the "interpretter"
-  (run-lisp "/p/tccdev/work_areas/vrayapp/tccdev/tcc_collateral/core/tcl/skill-repl.tcl"))
