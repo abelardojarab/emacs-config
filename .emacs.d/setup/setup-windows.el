80747cfbe807d00f7d691d843b2e0f14040b9f6b
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2017-07-14 13:15:54 -0700

    Update gnus configuration

--- .emacs.d/setup/setup-windows.el
+++ .emacs.d/setup/setup-windows.el
@@ -412,12 +412,11 @@ place the new frame at the right side of the current frame."
                       ("\\*Async             Shell.*\\*" :regexp  t                    :ignore t)
                       (occur-mode            :select     nil      :align               t)
                       ("*Help*"              :select     t        :inhibit-window-quit t       :other               t)
-                      ("*Completions*"       :size       0.3      :align               t       )
+                      ("*Completions*"       :size       0.3      :align               t)
                       ("*Messages*"          :select     nil      :inhibit-window-quit t       :other               t)
                       ("\\*[Wo]*Man.*\\*"    :regexp     t        :select              t       :inhibit-window-quit t :other t)
                       ("\\`\\*helm.*?\\*\\'" :regexp     t        :size                0.3     :align               t)
-                      ("*Calendar*"          :select     t        :size                0.3     :align               below)
-                      ))
+                      ("*Calendar*"          :select     t        :size                0.3     :align               below)))
 
               ;; Enable shackle
               (shackle-mode 1)))
