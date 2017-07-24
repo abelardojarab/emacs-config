99a0cd0f175b823452797d595dc4d9ca67bb712b
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2017-07-24 11:04:54 -0700

    Update dired and imenu integration

--- .emacs.d/setup/setup-dired.el
+++ .emacs.d/setup/setup-dired.el
@@ -37,6 +37,42 @@
               (setq-local truncate-lines t))
             (add-hook 'dired-mode-hook 'projectile-mode)
 
+            ;; https://fuco1.github.io/2017-05-01-Support-for-imenu-in-dired.html
+            (defun my/dired-imenu-prev-index-position (&optional arg)
+              "Go to the header line of previous directory."
+              (interactive "p")
+              (unless (= (line-number-at-pos) 1)
+                (call-interactively 'dired-prev-subdir)
+                t))
+
+            (defun my/dired-extract-index-name ()
+              "Extract name of the current item for imenu."
+              (save-excursion
+                (back-to-indentation)
+                (buffer-substring-no-properties
+                 (point)
+                 (1- (re-search-forward ":$")))))
+
+            (defun my/dired-imenu-create-index ()
+              "Create `imenu' index for dired."
+              (let* ((alist (imenu-default-create-index-function))
+                     (uniquified (f-uniquify-alist (-map 'car alist))))
+                (--remove
+                 (= 0 (length (car it)))
+                 (--map (cons (cdr (assoc (car it) uniquified)) (cdr it))
+                        alist))))
+
+            (defun my/dired-imenu-init ()
+              "Initialize `imenu' variables in current buffer."
+              (setq-local imenu-prev-index-position-function
+                          'my/dired-imenu-prev-index-position)
+              (setq-local imenu-extract-index-name-function
+                          'my/dired-extract-index-name)
+              (setq-local imenu-create-index-function
+                          'my/dired-imenu-create-index))
+
+            (add-hook 'dired-mode-hook 'my/dired-imenu-init)
+
             ;; Emacs 24.4 defaults to an ls -1 view, not ls -l, but I want
             ;; details. This needs to be specified before requiring dired+ (see
             ;; http://irreal.org/blog/?p=3341)
@@ -78,21 +114,21 @@
               (let ((files (dired-get-marked-files
                             nil current-prefix-arg))
                     ;; the rsync command
-                    (tmtxt/rsync-command
+                    (my/rsync-command
                      "rsync -arvz --progress "))
                 ;; add all selected file names as arguments
                 ;; to the rsync command
                 (dolist (file files)
-                  (setq tmtxt/rsync-command
-                        (concat tmtxt/rsync-command
+                  (setq my/rsync-command
+                        (concat my/rsync-command
                                 (shell-quote-argument file)
                                 " ")))
                 ;; append the destination
-                (setq tmtxt/rsync-command
-                      (concat tmtxt/rsync-command
+                (setq my/rsync-command
+                      (concat my/rsync-command
                               (shell-quote-argument dest)))
                 ;; run the async shell command
-                (async-shell-command tmtxt/rsync-command "*rsync*")
+                (async-shell-command my/rsync-command "*rsync*")
                 ;; finally, switch to that window
                 (other-window 1)))
 
