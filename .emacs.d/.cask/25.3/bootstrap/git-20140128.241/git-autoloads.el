58a4d6f85d8faac71b0c51ff5d246df6d491fb11
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2017-11-10 23:30:44 -0800

    Use cask to fetch packages

---
+++ .emacs.d/.cask/25.3/bootstrap/git-20140128.241/git-autoloads.el
@@ -0,0 +1,15 @@
+;;; git-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("git.el") (23046 42423 527267 443000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; git-autoloads.el ends here
