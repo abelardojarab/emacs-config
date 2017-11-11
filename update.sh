972df1fec094ed59e1c889f294dee5d0b636d511
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2017-11-11 00:23:31 -0800

    Add Cask packages

--- update.sh
+++ update.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-git subtree pull --prefix=.emacs.d/cmake-ide --squash https://github.com/abelardojarab/cmake-ide.git master
-git subtree pull --prefix=.emacs.d/helm  --squash https://github.com/abelardojarab/helm.git master
-git subtree pull --prefix=.emacs.d/magit  --squash https://github.com/abelardojarab/magit.git master
-git subtree pull --prefix=.emacs.d/flycheck  --squash https://github.com/abelardojarab/flycheck.git master
+# git subtree pull --prefix=.emacs.d/cmake-ide --squash https://github.com/abelardojarab/cmake-ide.git master
+# git subtree pull --prefix=.emacs.d/helm  --squash https://github.com/abelardojarab/helm.git master
+# git subtree pull --prefix=.emacs.d/magit  --squash https://github.com/abelardojarab/magit.git master
+# git subtree pull --prefix=.emacs.d/flycheck  --squash https://github.com/abelardojarab/flycheck.git master
+# git subtree pull --prefix=.emacs.d/yasnippet  --squash https://github.com/abelardojarab/yasnippet.git master
+# git subtree pull --prefix=.emacs.d/swiper  --squash https://github.com/abelardojarab/swiper.git master
+
+# Forked packages
 git subtree pull --prefix=.emacs.d/company-mode  --squash https://github.com/abelardojarab/company-mode.git master
-git subtree pull --prefix=.emacs.d/yasnippet  --squash https://github.com/abelardojarab/yasnippet.git master
-git subtree pull --prefix=.emacs.d/swiper  --squash https://github.com/abelardojarab/swiper.git master
 git subtree pull --prefix=.emacs.d/ecb  --squash https://github.com/abelardojarab/ecb.git master
 git subtree pull --prefix=.emacs.d/ergoemacs-mode  --squash https://github.com/abelardojarab/ergoemacs-mode.git master
 git subtree pull --prefix=.emacs.d/rtags  --squash https://github.com/abelardojarab/rtags.git master
