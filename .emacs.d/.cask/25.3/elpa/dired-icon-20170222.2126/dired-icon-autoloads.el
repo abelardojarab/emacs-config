7032418e71cbc0ba3005bb2c29fbc386321a4c3a
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2017-11-11 16:19:22 -0800

    Add dired-icon from melpa

---
+++ .emacs.d/.cask/25.3/elpa/dired-icon-20170222.2126/dired-icon-autoloads.el
@@ -0,0 +1,27 @@
+;;; dired-icon-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "dired-icon" "dired-icon.el" (23047 37749 594975
+;;;;;;  932000))
+;;; Generated autoloads from dired-icon.el
+
+(autoload 'dired-icon-mode "dired-icon" "\
+Display icons according to the file types in dired buffers.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("dired-icon-pkg.el") (23047 37749 593975
+;;;;;;  953000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dired-icon-autoloads.el ends here
---
+++ .emacs.d/.cask/25.3/elpa/dired-icon-20170222.2126/dired-icon-pkg.el
@@ -0,0 +1,7 @@
+(define-package "dired-icon" "20170222.2126" "A minor mode to display a list of associated icons in dired buffers."
+  '((emacs "24.3"))
+  :url "https://gitlab.com/xuhdev/dired-icon" :keywords
+  '("dired" "files"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
