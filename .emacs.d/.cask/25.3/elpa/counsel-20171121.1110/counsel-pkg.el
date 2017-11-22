66c4c85e141409a7f5ca117a0db61c5cee4b32b3
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2017-11-22 14:43:52 -0800

    Update packages from melpa

---
+++ .emacs.d/.cask/25.3/elpa/counsel-20171121.1110/counsel-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel" "20171121.1110" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.9.0")) :commit "5b270c6031cf43a165f3a53a0f15bb1782cdf585" :url "https://github.com/abo-abo/swiper" :keywords '("completion" "matching"))
---
+++ .emacs.d/.cask/25.3/elpa/helm-tramp-20171120.140/helm-tramp-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-tramp" "20171120.140" "Tramp helm interface for ssh, docker, vagrant" '((emacs "24.3") (helm "2.0")) :commit "d90be189d8c2b742c9621ff28b1196be683cdb4c" :url "https://github.com/masasam/emacs-helm-tramp")
