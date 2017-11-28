09970c493d10a0b3b71efa3cd1f63a4d5b252c42
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2017-11-28 15:54:23 -0800

    update packages from melpa

---
+++ .emacs.d/.cask/25.3/elpa/python-mode-20171126.1618/python-mode-pkg.el
@@ -0,0 +1,4 @@
+(define-package "python-mode" "20171126.1618" "Python major mode" 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ .emacs.d/.cask/25.3/elpa/use-package-20171128.1141/use-package-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "use-package" "20171128.1141" "A use-package declaration for simplifying your .emacs" '((bind-key "1.0") (diminish "0.44")) :commit "6a9c8852d3c3747a01ed1f118434c3bfd686e481" :url "https://github.com/jwiegley/use-package" :keywords '("dotemacs" "startup" "speed" "config" "package"))
