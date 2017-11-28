f92d4e5aa4a73b0e8e3086ee5561f348c2f8a0e1
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2017-11-27 16:29:27 -0800

    updates from melpa

---
+++ .emacs.d/.cask/25.3/elpa/counsel-20171126.239/counsel-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel" "20171126.239" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.9.0")) :commit "ce2ffaa061df40a1d2dc7124cf66d957cd0d0e58" :url "https://github.com/abo-abo/swiper" :keywords '("completion" "matching"))
---
+++ .emacs.d/.cask/25.3/elpa/zerodark-theme-20171126.1202/zerodark-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "zerodark-theme" "20171126.1202" "A dark, medium contrast theme for Emacs" '((all-the-icons "2.0.0") (magit "2.8.0") (flycheck "29")) :commit "f4eacff7f9d612ab5c6a49849ddb11d0f74887bb" :url "https://github.com/NicolasPetton/zerodark-theme" :keywords '("themes"))
