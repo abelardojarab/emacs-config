ea76905a9357499ec85d22aa5b78a1c15690f41b
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2017-12-06 11:00:36 -0800

    updates from melpa

---
+++ .emacs.d/.cask/25.3/elpa/helm-20171206.311/helm-pkg.el
@@ -0,0 +1,9 @@
+(define-package "helm" "20171206.311" "Helm is an Emacs incremental and narrowing framework"
+  '((emacs "24.4")
+    (async "1.9.2")
+    (popup "0.5.3")
+    (helm-core "2.8.6"))
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
