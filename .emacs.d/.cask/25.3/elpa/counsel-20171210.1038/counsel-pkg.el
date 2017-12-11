bcf3d3aeb98a98456c161cfb3bc59a1c4c464dc0
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2017-12-11 07:35:08 -0800

    updates from melpa

---
+++ .emacs.d/.cask/25.3/elpa/counsel-20171210.1038/counsel-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel" "20171210.1038" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.9.0")) :commit "5c096a2ca67b954e1d1a80716c405270bf118bff" :url "https://github.com/abo-abo/swiper" :keywords '("completion" "matching"))
---
+++ .emacs.d/.cask/25.3/elpa/org-20171211/org-pkg.el
@@ -0,0 +1,3 @@
+(define-package "org"
+  "20171211" "Outline-based notes management and organizer" ( ))
+;; no-byte-compile: t
