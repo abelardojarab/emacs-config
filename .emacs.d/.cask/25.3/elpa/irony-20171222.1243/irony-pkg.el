34912b0804de2378c3d9a8630117473d39911faf
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2017-12-22 23:09:07 -0800

    use local irony

--- .emacs.d/.cask/25.3/elpa/irony-20171222.1243/irony-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "irony" "20171222.1243" "C/C++ minor mode powered by libclang"
-  '((cl-lib "0.5")
-    (json "1.2"))
-  :url "https://github.com/Sarcasm/irony-mode" :keywords
-  '("c" "convenience" "tools"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
