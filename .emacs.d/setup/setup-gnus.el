e91700994b913782f99085116002507e8a7e4cc3
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2018-01-12 22:51:07 -0800

    update email

--- .emacs.d/setup/setup-gnus.el
+++ .emacs.d/setup/setup-gnus.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2014, 2015, 2016, 2017, 2018  Abelardo Jara-Berrocal
 
-;; Author: Abelardo Jara-Berrocal <abelardojara@Abelardos-MacBook-Pro.local>
+;; Author: Abelardo Jara-Berrocal <abelardojarab@gmail.com>
 ;; Keywords:
 
 ;; This program is free software; you can redistribute it and/or modify
