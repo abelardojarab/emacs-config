d9f1a51f56d4a5c0dfd4f8b682b10587b82c06f4
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2018-01-14 14:18:31 -0800

    updates from melpa

--- update.sh
+++ update.sh
@@ -3,7 +3,6 @@
 # Forked packages
 
 git subtree pull --prefix=.emacs.d/yasnippet  --squash https://github.com/abelardojarab/yasnippet.git master
-git subtree pull --prefix=.emacs.d/ergoemacs-mode  --squash https://github.com/abelardojarab/ergoemacs-mode.git master
 git subtree pull --prefix=.emacs.d/rtags  --squash https://github.com/abelardojarab/rtags.git master
 git subtree pull --prefix .emacs.d/rtags/src/rct --squash https://github.com/Andersbakken/rct master
 git subtree pull --prefix .emacs.d/rtags/src/selene --squash https://github.com/jeremyong/selene master
