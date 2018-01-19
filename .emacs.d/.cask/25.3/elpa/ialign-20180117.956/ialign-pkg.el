152af81e9d22585ac86c5f651e0f0a4e0f73ff92
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2018-01-19 09:45:41 -0800

    update packages from melpa

---
+++ .emacs.d/.cask/25.3/elpa/ialign-20180117.956/ialign-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ialign" "20180117.956" "visual align-regexp" '((emacs "24.4")) :commit "1d00ab870d06b946d94e5e6d340b85a3e51fbfb1" :url "https://github.com/mkcms/interactive-align" :keywords '("tools" "editing" "align" "interactive"))
