d45053d5f4690265be29ce9dac4ad68b99700917
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2018-01-25 13:16:49 -0800

    updates from melpa

---
+++ .emacs.d/.cask/25.3/elpa/org-ref-20180122.1906/org-ref-pkg.el
@@ -0,0 +1,16 @@
+(define-package "org-ref" "20180122.1906" "citations, cross-references and bibliographies in org-mode"
+  '((dash "2.11.0")
+    (helm "1.5.5")
+    (helm-bibtex "2.0.0")
+    (ivy "0.8.0")
+    (hydra "0.13.2")
+    (key-chord "0")
+    (s "1.10.0")
+    (f "0.18.0")
+    (emacs "24.4")
+    (pdf-tools "0.7"))
+  :url "https://github.com/jkitchin/org-ref" :keywords
+  '("org-mode" "cite" "ref" "label"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ .emacs.d/.cask/25.3/elpa/zerodark-theme-20180125.739/zerodark-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "zerodark-theme" "20180125.739" "A dark, medium contrast theme for Emacs" '((all-the-icons "2.0.0") (magit "2.8.0") (flycheck "29")) :commit "f22ea6ed957440dccbb4a21bacc545d27830423e" :url "https://github.com/NicolasPetton/zerodark-theme" :keywords '("themes"))
