35138ae4f54a58070c9300f6cb2423d69ca79939
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2018-01-30 13:29:44 -0800

    update packages

--- .emacs.d/.cask/25.3/elpa/ht-20171213.1334/ht-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; ht-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("ht.el") (23092 22109 234535 23000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; ht-autoloads.el ends here
--- .emacs.d/.cask/25.3/elpa/ivy-rich-20180109.1933/ivy-rich-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ivy-rich" "20180109.1933" "More friendly display transformer for ivy." '((emacs "24.4") (ivy "0.8.0")) :commit "efe35d2f579202ca14a90cfd46ecac624109558c" :keywords '("ivy"))
