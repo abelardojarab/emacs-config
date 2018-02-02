b2a1ae6caa4a84748dc2f000d16f2541ca6bd11a
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2018-02-02 12:55:49 -0800

    add cmake project

---
+++ .emacs.d/.cask/25.3/elpa/cmake-project-20171121.315/cmake-project-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cmake-project" "20171121.315" "Integrates CMake build process with Emacs" 'nil :commit "d3f408f226eff3f77f7e00dd519f4efc78fd292d" :url "http://github.com/alamaison/emacs-cmake-project" :keywords '("c" "cmake" "languages" "tools"))
