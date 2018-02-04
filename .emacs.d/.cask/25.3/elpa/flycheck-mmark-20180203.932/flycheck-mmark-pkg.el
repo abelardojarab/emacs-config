65fcf9aa731436aebb2320b525aa035e2d0594b1
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2018-02-03 22:10:47 -0800

    update package

---
+++ .emacs.d/.cask/25.3/elpa/flycheck-mmark-20180203.932/flycheck-mmark-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flycheck-mmark" "20180203.932" "Flycheck checker for the MMark markdown processor" '((emacs "24.4") (flycheck "0.29")) :commit "7fdcc48ff6ffa5e7db126a76f4948ab08b9eb8d4" :url "https://github.com/mmark-md/flycheck-mmark" :keywords '("convenience" "text"))
