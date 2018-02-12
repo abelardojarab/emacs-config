8b4ff3ffcfd52db6ad55507a9f5ec0b39e08e9c6
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2018-02-12 14:22:26 -0800

    update form melpa

---
+++ .emacs.d/.cask/25.3/elpa/swiper-20180211.1018/swiper-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "swiper" "20180211.1018" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.9.0")) :commit "cc5197d5de78ba981df54815c8615d795e0ffdc5" :url "https://github.com/abo-abo/swiper" :keywords '("matching"))
--- .emacs.d/.cask/25.3/elpa/web-mode-20180210.28/web-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "web-mode" "20180210.28" "major mode for editing web templates" '((emacs "23.1")) :commit "7f4947adc26fea6035b380f1b9d8a6ae6a2109b2" :url "http://web-mode.org" :keywords '("languages"))
