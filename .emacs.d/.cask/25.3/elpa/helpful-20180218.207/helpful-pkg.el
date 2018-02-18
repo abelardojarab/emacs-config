d62ad869ad6604965a42940109fa1c03a278fb1d
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2018-02-18 10:36:25 -0800

    updates from melpa

---
+++ .emacs.d/.cask/25.3/elpa/helpful-20180218.207/helpful-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helpful" "20180218.207" "a better *help* buffer" '((emacs "25.1") (dash "2.12.0") (dash-functional "1.2.0") (s "1.11.0") (f "0.20.0") (elisp-refs "1.2") (shut-up "0.3")) :commit "c5a5965017606fa703cdfabdac1441483ad72e9c" :url "https://github.com/Wilfred/helpful" :keywords '("help" "lisp"))
