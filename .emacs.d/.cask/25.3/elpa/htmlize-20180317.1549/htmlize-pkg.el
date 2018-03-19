82b29912204e6b5d744d184cc8ca70471a815fe6
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2018-03-19 13:23:21 -0700

    update from melpa

---
+++ .emacs.d/.cask/25.3/elpa/htmlize-20180317.1549/htmlize-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "htmlize" "20180317.1549" "Convert buffer text and decorations to HTML." 'nil :commit "75c21c7f4ddff0d1bbdf1b4f66a947368a6f3188" :keywords '("hypermedia" "extensions"))
---
+++ .emacs.d/.cask/25.3/elpa/keychain-environment-20180318.1523/keychain-environment-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "keychain-environment" "20180318.1523" "load keychain environment variables" 'nil :commit "d3643196de6dc79ea77f9f4805028350fd76100b" :url "https://github.com/tarsius/keychain-environment" :keywords '("gnupg" "pgp" "ssh"))
