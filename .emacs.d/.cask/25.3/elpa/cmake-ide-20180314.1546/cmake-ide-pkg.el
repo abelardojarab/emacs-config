3470cf28f7aca23e1c2e56182135acadd8624895
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2018-03-22 17:06:11 -0700

    update from melpa

--- .emacs.d/.cask/25.3/elpa/cmake-ide-20180314.1546/cmake-ide-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "cmake-ide" "20180314.1546" "Calls CMake to find out include paths and other compiler flags" '((emacs "24.4") (cl-lib "0.5") (seq "1.11") (levenshtein "0") (s "1.11.0")) :commit "3caa4ac086db8818d4005ad023a5432e83ff4b8c" :url "http://github.com/atilaneves/cmake-ide" :keywords '("languages"))
---
+++ .emacs.d/.cask/25.3/elpa/elscreen-20180320.1902/elscreen-pkg.el
@@ -0,0 +1,7 @@
+(define-package "elscreen" "20180320.1902" "Emacs window session manager"
+  '((emacs "24"))
+  :url "https://github.com/knu/elscreen" :keywords
+  '("window" "convenience"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
