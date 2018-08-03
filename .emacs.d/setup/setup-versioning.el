ec8496761f65ed42f6b8a980b0b320f8c6414b9f
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2018-08-02 19:14:00 -0700

    add smerge

--- .emacs.d/setup/setup-versioning.el
+++ .emacs.d/setup/setup-versioning.el
@@ -487,7 +487,7 @@
               (interactive)
               (git-timemachine--start #'my/git-timemachine-show-selected-revision))))
 
-;; Show blame for current line
+;; Show blame for current lines
 (use-package git-messenger
   :if (executable-find "git")
   :commands (git-messenger:popup-message)
@@ -559,11 +559,67 @@
                         ;; Start git gutter mode
                         (global-git-gutter-mode t)))))
 
-;; highlight regions according to age
+;; Highlight regions according to age
 (use-package smeargle
+  :defer t
   :if (executable-find "git")
   :commands (smeargle smeargle-commits smeargle-clear)
   :load-path (lambda () (expand-file-name "smeargle/" user-emacs-directory)))
+
+;; Smerge, resolve merge conflicts
+(use-package smerge-mode
+  :defer t
+  :if (executable-find "git")
+  :commands (hydra-smerge/body smerge-mode)
+  :init (progn
+    (defun my/enable-smerge-maybe ()
+      "Auto-enable `smerge-mode' when merge conflict is detected."
+      (save-excursion
+        (goto-char (point-min))
+        (when (re-search-forward "^<<<<<<< " nil :noerror)
+          (smerge-mode 1))))
+    (add-hook 'find-file-hook #'my/enable-smerge-maybe :append))
+  :config (progn
+            (>=e "26.0"
+                 nil
+                 (defalias 'smerge-keep-upper 'smerge-keep-mine)
+                 (defalias 'smerge-keep-lower 'smerge-keep-other)
+                 (defalias 'smerge-diff-base-upper 'smerge-diff-base-mine)
+                 (defalias 'smerge-diff-upper-lower 'smerge-diff-mine-other)
+                 (defalias 'smerge-diff-base-lower 'smerge-diff-base-other))
+
+            (defhydra hydra-smerge (:color pink
+                                           :hint nil
+                                           :pre (smerge-mode 1)
+                                           ;; Disable `smerge-mode' when quitting hydra if
+                                           ;; no merge conflicts remain.
+                                           :post (smerge-auto-leave))
+              "
+^Move^       ^Keep^               ^Diff^                 ^Other^
+^^-----------^^-------------------^^---------------------^^-------
+_n_ext       _b_ase               _<_: upper/base        _C_ombine
+_p_rev       _u_pper              _=_: upper/lower       _r_esolve
+^^           _l_ower              _>_: base/lower        _k_ill current
+^^           _a_ll                _R_efine
+^^           _RET_: current       _E_diff
+"
+              ("n" smerge-next)
+              ("p" smerge-prev)
+              ("b" smerge-keep-base)
+              ("u" smerge-keep-upper)
+              ("l" smerge-keep-lower)
+              ("a" smerge-keep-all)
+              ("RET" smerge-keep-current)
+              ("\C-m" smerge-keep-current)
+              ("<" smerge-diff-base-upper)
+              ("=" smerge-diff-upper-lower)
+              (">" smerge-diff-base-lower)
+              ("R" smerge-refine)
+              ("E" smerge-ediff)
+              ("C" smerge-combine-with-next)
+              ("r" smerge-resolve)
+              ("k" smerge-kill-current)
+              ("q" nil "cancel" :color blue))))
 
 (provide 'setup-versioning)
 ;;; setup-versioning.el ends here
