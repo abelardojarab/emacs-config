4d42072540b34dae446d236416fe5cc46bbe0497
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2018-08-13 13:35:45 -0700

    Continue feature: Update copyright.

--- .emacs.d/setup/setup-regex.el
+++ .emacs.d/setup/setup-regex.el
@@ -1,6 +1,6 @@
 ;;; setup-regex.el ---                               -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2016, 2018  Abelardo Jara-Berrocal
+;; Copyright (C) 2014-2018  Abelardo Jara-Berrocal
 
 ;; Author: Abelardo Jara-Berrocal <abelardojarab@gmail.com>
 ;; Keywords:
