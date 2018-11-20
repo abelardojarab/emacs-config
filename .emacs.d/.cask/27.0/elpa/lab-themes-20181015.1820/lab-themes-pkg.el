30ed0ee7eed96ed63774efa017f3c6b38e92f78c
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2018-11-19 22:06:16 -0800

    Fix indentation

---
+++ .emacs.d/.cask/27.0/elpa/lab-themes-20181015.1820/lab-themes-pkg.el
@@ -0,0 +1,12 @@
+(define-package "lab-themes" "20181015.1820" "A custom theme carefully constructed in the LAB space"
+  '((emacs "24"))
+  :keywords
+  '("lisp")
+  :authors
+  '(("MetroWind" . "chris.corsair@gmail.com"))
+  :maintainer
+  '("MetroWind" . "chris.corsair@gmail.com")
+  :url "https://github.com/MetroWind/lab-theme")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
