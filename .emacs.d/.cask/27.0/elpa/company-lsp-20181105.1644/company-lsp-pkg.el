87d3eeb871999cd5960ef8695b3150d1a5a31a53
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2018-11-30 11:12:04 -0800

    add new packages

---
+++ .emacs.d/.cask/27.0/elpa/company-lsp-20181105.1644/company-lsp-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "company-lsp" "20181105.1644" "Company completion backend for lsp-mode." '((emacs "25.1") (lsp-mode "3.4") (company "0.9.0") (s "1.2.0") (dash "2.11.0")) :commit "d333e5594f8d5e5cb96309f8a913747ff83ab089" :url "https://github.com/tigersoldier/company-lsp")
---
+++ .emacs.d/.cask/27.0/elpa/lsp-ui-20181128.616/lsp-ui-pkg.el
@@ -0,0 +1,17 @@
+(define-package "lsp-ui" "20181128.616" "UI modules for lsp-mode"
+  '((emacs "25.1")
+    (dash "2.14")
+    (dash-functional "1.2.0")
+    (flycheck "31")
+    (lsp-mode "5.0")
+    (markdown-mode "2.3"))
+  :keywords
+  '("lsp")
+  :authors
+  '(("Sebastien Chapuis <sebastien@chapu.is>, Fangrui Song" . "i@maskray.me"))
+  :maintainer
+  '("Sebastien Chapuis <sebastien@chapu.is>, Fangrui Song" . "i@maskray.me")
+  :url "https://github.com/emacs-lsp/lsp-ui")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
