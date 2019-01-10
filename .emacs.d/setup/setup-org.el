b34df9d7829db19293f9e03522de8ad297be6576
Abelardo Jara-Berrocal <abelardo.jara-b@meghcomputing.com>
2019-01-10 09:37:25 -0800

    Fix issue with org-mode

--- .emacs.d/setup/setup-org.el
+++ .emacs.d/setup/setup-org.el
@@ -26,7 +26,7 @@
 
 ;; Org mode
 (use-package org
-  :defer t
+  :demand t
   :mode (("\\.org$" . org-mode))
   :load-path (lambda () (expand-file-name "org/lisp" user-emacs-directory))
   :bind (("C-c C"        . org-capture)
