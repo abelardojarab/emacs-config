f52449dff0812851809eb59c5b01a293945d574a
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2019-08-01 09:59:07 -0700

    update

---
+++ .emacs.d/.cask/27.0/elpa/parchment-theme-20190703.1531/parchment-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/abelardojara/workspace/emacs-config/.emacs.d/.cask/27.0/elpa/parchment-theme-20190703.1531/parchment-theme.el  -*- no-byte-compile: t -*-
+(define-package "parchment-theme" "20190703.1531" "Light theme inspired by Acme and Leuven" '((emacs "24")) :commit "30eedd346175072e013b7bcf6ae1795f022fee0b" :authors '(("Alex Griffin" . "a@ajgrf.com")) :maintainer '("Alex Griffin" . "a@ajgrf.com") :url "https://github.com/ajgrf/parchment")
