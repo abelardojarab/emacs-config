d53f7496ea1a466259ed5f51487ce513de16abde
Abelardo Jara-Berrocal <abelardoj@nvidia.com>
2020-01-03 12:10:39 -0800

    Add ripgrep

---
+++ .emacs.d/.cask/27.0/elpa/deadgrep-20191031.2214/deadgrep-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/abelardojara/workspace/emacs-config/.emacs.d/.cask/27.0/elpa/deadgrep-20191031.2214/deadgrep.el  -*- no-byte-compile: t -*-
+(define-package "deadgrep" "20191031.2214" "fast, friendly searching with ripgrep" '((emacs "25.1") (dash "2.12.0") (s "1.11.0") (spinner "1.7.3")) :commit "6ea9ad874827e7ba6c8f5e62a89a5a479cd20735" :keywords '("tools") :authors '(("Wilfred Hughes" . "me@wilfred.me.uk")) :maintainer '("Wilfred Hughes" . "me@wilfred.me.uk") :url "https://github.com/Wilfred/deadgrep")
