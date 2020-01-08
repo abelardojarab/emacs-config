d1d8c3bfa0d7d603d436da69e9aad246733eaf4b
Abelardo Jara-Berrocal <abelardoj@nvidia.com>
2020-01-08 10:07:35 -0800

    Update setup to use customs more

--- .emacs.d/setup/setup-undoandredo.el
+++ .emacs.d/setup/setup-undoandredo.el
@@ -1,6 +1,6 @@
 ;;; setup-undoandredo.el ---                         -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2014-2018, 2020  Abelardo Jara-Berrocal
+;; Copyright (C) 2014-2020  Abelardo Jara-Berrocal
 
 ;; Author: Abelardo Jara-Berrocal <abelardojarab@gmail.com>
 ;; Keywords:
