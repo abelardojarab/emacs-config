387b59ab106c77f07d31c1c0daf2e2cd8f5ff604
Abelardo Jara-Berrocal <abelardoj@nvidia.com>
2020-01-23 10:41:38 -0800

    Add hledger

---
+++ .emacs.d/.cask/27.0/elpa/hledger-mode-20191012.1046/hledger-mode-pkg.el
@@ -0,0 +1,15 @@
+(define-package "hledger-mode" "20191012.1046" "A mode for writing journal entries for hledger."
+  '((emacs "24.4")
+    (popup "0.5.3")
+    (async "1.9")
+    (htmlize "1.47"))
+  :keywords
+  '("data")
+  :authors
+  '(("Narendra Joshi" . "narendraj9@gmail.com"))
+  :maintainer
+  '("Narendra Joshi" . "narendraj9@gmail.com")
+  :url "https://github.com/narendraj9/hledger-mode.git")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
