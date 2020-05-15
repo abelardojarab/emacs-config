62005aca64a92d3beec58fd8c0b5b1de5d6f3471
Abelardo Jara-Berrocal <abelardoj@nvidia.com>
2020-05-15 15:31:43 -0700

    use company-box only on emacs 27 or above

--- .emacs.d/setup/setup-company.el
+++ .emacs.d/setup/setup-company.el
@@ -31,8 +31,7 @@
              company-mode
              add-pcomplete-to-capf
              company-text-setup
-             company-c-setup
-             company-minibuffer-setup)
+             company-c-setup)
   :custom ((company-begin-commands           '(self-insert-command
                                                org-self-insert-command
                                                c-electric-lt-gt
@@ -179,22 +178,25 @@
 
             ;; Documentation popups for company
             (use-package company-box
-              :disabled t
+              ;; Disabled on emacs newer than 27.0
+              :if (not (version< emacs-version "27.0"))
+              :diminish t
               :functions (my/company-box--make-line
-                          my/company-box-icons--elisp)
-              :commands (company-box--get-color
-                         company-box--resolve-colors
-                         company-box--add-icon
-                         company-box--apply-color
-                         company-box--make-line
-                         company-box-icons--elisp)
+                          my/company-box-icons--elisp
+                          company-box--get-color
+                          company-box--resolve-colors
+                          company-box--add-icon
+                          company-box--apply-color
+                          company-box--make-line
+                          company-box-icons--elisp)
+              :defines company-box-icons-all-the-icons
               :hook (company-mode . company-box-mode)
+              :init (setq company-box-icons-alist 'company-box-icons-all-the-icons)
               :custom ((company-box-backends-colors       nil)
                        (company-box-show-single-candidate t)
                        (company-box-max-candidates        50)
                        (company-box-doc-delay             0.3))
               :config (progn
-                        ;; Support `company-common'
                         (defun my/company-box--make-line (candidate)
                           (-let* (((candidate annotation len-c len-a backend) candidate)
                                   (color (company-box--get-color backend))
@@ -233,40 +235,40 @@
                                     (t . nil)))))
                         (advice-add #'company-box-icons--elisp :override #'my/company-box-icons--elisp)
 
-                        (when (and (display-graphic-p)
-                                   (require 'all-the-icons nil t))
-                          (declare-function all-the-icons-faicon 'all-the-icons)
-                          (declare-function all-the-icons-material 'all-the-icons)
-                          (declare-function all-the-icons-octicon 'all-the-icons)
-                          (setq company-box-icons-all-the-icons
-                                `((Unknown . ,(all-the-icons-material "find_in_page" :height 0.85 :v-adjust -0.2))
-                                  (Text . ,(all-the-icons-faicon "text-width" :height 0.8 :v-adjust -0.05))
-                                  (Method . ,(all-the-icons-faicon "cube" :height 0.8 :v-adjust -0.05 :face 'all-the-icons-purple))
-                                  (Function . ,(all-the-icons-faicon "cube" :height 0.8 :v-adjust -0.05 :face 'all-the-icons-purple))
-                                  (Constructor . ,(all-the-icons-faicon "cube" :height 0.8 :v-adjust -0.05 :face 'all-the-icons-purple))
-                                  (Field . ,(all-the-icons-octicon "tag" :height 0.8 :v-adjust 0 :face 'all-the-icons-lblue))
-                                  (Variable . ,(all-the-icons-octicon "tag" :height 0.8 :v-adjust 0 :face 'all-the-icons-lblue))
-                                  (Class . ,(all-the-icons-material "settings_input_component" :height 0.85 :v-adjust -0.2 :face 'all-the-icons-orange))
-                                  (Interface . ,(all-the-icons-material "share" :height 0.85 :v-adjust -0.2 :face 'all-the-icons-lblue))
-                                  (Module . ,(all-the-icons-material "view_module" :height 0.85 :v-adjust -0.2 :face 'all-the-icons-lblue))
-                                  (Property . ,(all-the-icons-faicon "wrench" :height 0.8 :v-adjust -0.05))
-                                  (Unit . ,(all-the-icons-material "settings_system_daydream" :height 0.85 :v-adjust -0.2))
-                                  (Value . ,(all-the-icons-material "format_align_right" :height 0.85 :v-adjust -0.2 :face 'all-the-icons-lblue))
-                                  (Enum . ,(all-the-icons-material "storage" :height 0.85 :v-adjust -0.2 :face 'all-the-icons-orange))
-                                  (Keyword . ,(all-the-icons-material "filter_center_focus" :height 0.85 :v-adjust -0.2))
-                                  (Snippet . ,(all-the-icons-material "format_align_center" :height 0.85 :v-adjust -0.2))
-                                  (Color . ,(all-the-icons-material "palette" :height 0.85 :v-adjust -0.2))
-                                  (File . ,(all-the-icons-faicon "file-o" :height 0.85 :v-adjust -0.05))
-                                  (Reference . ,(all-the-icons-material "collections_bookmark" :height 0.85 :v-adjust -0.2))
-                                  (Folder . ,(all-the-icons-faicon "folder-open" :height 0.85 :v-adjust -0.05))
-                                  (EnumMember . ,(all-the-icons-material "format_align_right" :height 0.85 :v-adjust -0.2 :face 'all-the-icons-lblue))
-                                  (Constant . ,(all-the-icons-faicon "square-o" :height 0.85 :v-adjust -0.05))
-                                  (Struct . ,(all-the-icons-material "settings_input_component" :height 0.85 :v-adjust -0.2 :face 'all-the-icons-orange))
-                                  (Event . ,(all-the-icons-faicon "bolt" :height 0.8 :v-adjust -0.05 :face 'all-the-icons-orange))
-                                  (Operator . ,(all-the-icons-material "control_point" :height 0.85 :v-adjust -0.2))
-                                  (TypeParameter . ,(all-the-icons-faicon "arrows" :height 0.8 :v-adjust -0.05))
-                                  (Template . ,(all-the-icons-material "format_align_center" :height 0.85 :v-adjust -0.2)))
-                                company-box-icons-alist 'company-box-icons-all-the-icons))))
+                        (when (display-graphic-p)
+                          (with-eval-after-load 'all-the-icons
+                            (declare-function all-the-icons-faicon 'all-the-icons)
+                            (declare-function all-the-icons-material 'all-the-icons)
+                            (declare-function all-the-icons-octicon 'all-the-icons)
+                            (setq company-box-icons-all-the-icons
+                                  `((Unknown . ,(all-the-icons-material "find_in_page" :height 0.85 :v-adjust -0.2))
+                                    (Text . ,(all-the-icons-faicon "text-width" :height 0.8 :v-adjust -0.05))
+                                    (Method . ,(all-the-icons-faicon "cube" :height 0.8 :v-adjust -0.05 :face 'all-the-icons-purple))
+                                    (Function . ,(all-the-icons-faicon "cube" :height 0.8 :v-adjust -0.05 :face 'all-the-icons-purple))
+                                    (Constructor . ,(all-the-icons-faicon "cube" :height 0.8 :v-adjust -0.05 :face 'all-the-icons-purple))
+                                    (Field . ,(all-the-icons-octicon "tag" :height 0.8 :v-adjust 0 :face 'all-the-icons-lblue))
+                                    (Variable . ,(all-the-icons-octicon "tag" :height 0.8 :v-adjust 0 :face 'all-the-icons-lblue))
+                                    (Class . ,(all-the-icons-material "settings_input_component" :height 0.85 :v-adjust -0.2 :face 'all-the-icons-orange))
+                                    (Interface . ,(all-the-icons-material "share" :height 0.85 :v-adjust -0.2 :face 'all-the-icons-lblue))
+                                    (Module . ,(all-the-icons-material "view_module" :height 0.85 :v-adjust -0.2 :face 'all-the-icons-lblue))
+                                    (Property . ,(all-the-icons-faicon "wrench" :height 0.8 :v-adjust -0.05))
+                                    (Unit . ,(all-the-icons-material "settings_system_daydream" :height 0.85 :v-adjust -0.2))
+                                    (Value . ,(all-the-icons-material "format_align_right" :height 0.85 :v-adjust -0.2 :face 'all-the-icons-lblue))
+                                    (Enum . ,(all-the-icons-material "storage" :height 0.85 :v-adjust -0.2 :face 'all-the-icons-orange))
+                                    (Keyword . ,(all-the-icons-material "filter_center_focus" :height 0.85 :v-adjust -0.2))
+                                    (Snippet . ,(all-the-icons-material "format_align_center" :height 0.85 :v-adjust -0.2))
+                                    (Color . ,(all-the-icons-material "palette" :height 0.85 :v-adjust -0.2))
+                                    (File . ,(all-the-icons-faicon "file-o" :height 0.85 :v-adjust -0.05))
+                                    (Reference . ,(all-the-icons-material "collections_bookmark" :height 0.85 :v-adjust -0.2))
+                                    (Folder . ,(all-the-icons-faicon "folder-open" :height 0.85 :v-adjust -0.05))
+                                    (EnumMember . ,(all-the-icons-material "format_align_right" :height 0.85 :v-adjust -0.2 :face 'all-the-icons-lblue))
+                                    (Constant . ,(all-the-icons-faicon "square-o" :height 0.85 :v-adjust -0.05))
+                                    (Struct . ,(all-the-icons-material "settings_input_component" :height 0.85 :v-adjust -0.2 :face 'all-the-icons-orange))
+                                    (Event . ,(all-the-icons-faicon "bolt" :height 0.8 :v-adjust -0.05 :face 'all-the-icons-orange))
+                                    (Operator . ,(all-the-icons-material "control_point" :height 0.85 :v-adjust -0.2))
+                                    (TypeParameter . ,(all-the-icons-faicon "arrows" :height 0.8 :v-adjust -0.05))
+                                    (Template . ,(all-the-icons-material "format_align_center" :height 0.85 :v-adjust -0.2)))
+                                  company-box-icons-alist 'company-box-icons-all-the-icons)))))
 
             ;; Documentation popups for company
             (use-package company-quickhelp
