792100437cd44abd10f36ec8a2a26d26fce7acdd
Abelardo Jara-Berrocal <abelardoj@nvidia.com>
2020-08-12 11:58:30 -0700

    posframe packages

---
+++ .emacs.d/.cask/27.0/elpa/helm-posframe-20200512.1146/helm-posframe-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/abelardojara/workspace/emacs-config/.emacs.d/.cask/27.0/elpa/helm-posframe-20200512.1146/helm-posframe.el  -*- no-byte-compile: t -*-
+(define-package "helm-posframe" "20200512.1146" "Using posframe to show helm window" '((emacs "26.0") (posframe "0.1.0") (helm "0.1")) :commit "b107e64eedef6292c49d590f30d320c29b64190b" :keywords '("abbrev" "convenience" "matching" "helm") :authors '(("Feng Shu")) :maintainer '("Feng Shu" . "tumashu@163.com") :url "https://github.com/tumashu/helm-posframe")
