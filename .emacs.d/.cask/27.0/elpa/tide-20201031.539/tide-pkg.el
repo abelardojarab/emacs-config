9f860a7012c74e7a5358ab5a8ad8d76a052c4136
Abelardo Jara-Berrocal <abelardoj@nvidia.com>
2020-12-04 14:43:45 -0800

    Updates

---
+++ .emacs.d/.cask/27.0/elpa/tide-20201031.539/tide-pkg.el
@@ -0,0 +1,17 @@
+(define-package "tide" "20201031.539" "Typescript Interactive Development Environment"
+  '((emacs "25.1")
+    (dash "2.10.0")
+    (s "1.11.0")
+    (flycheck "27")
+    (typescript-mode "0.1")
+    (cl-lib "0.5"))
+  :commit "a43a37bfa23bd482cd96e55c0c39973f83128c57" :keywords
+  ("typescript")
+  :authors
+  (("Anantha kumaran" . "ananthakumaran@gmail.com"))
+  :maintainer
+  ("Anantha kumaran" . "ananthakumaran@gmail.com")
+  :url "http://github.com/ananthakumaran/tide")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
