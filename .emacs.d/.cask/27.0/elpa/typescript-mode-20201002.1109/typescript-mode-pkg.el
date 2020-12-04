4c7d995c893730fa83a575fee5e8411013694e8c
Abelardo Jara-Berrocal <abelardoj@nvidia.com>
2020-12-04 12:21:50 -0800

    new packages

---
+++ .emacs.d/.cask/27.0/elpa/typescript-mode-20201002.1109/typescript-mode-pkg.el
@@ -0,0 +1,8 @@
+(define-package "typescript-mode" "20201002.1109" "Major mode for editing typescript"
+  '((emacs "24.3"))
+  :commit "54f14c482701c4f937bf51469f70812624e07f87" :keywords
+  ("typescript" "languages")
+  :url "http://github.com/ananthakumaran/typescript.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
