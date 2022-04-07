2028af9f801a23198f8bc5c9b11270535b4929da
Abelardo Jara-Berrocal <abelardojarab@gmail.com>
2022-04-07 10:55:56 -0700

    Add emacs 28.1 files

---
+++ .emacs.d/.cask/28.1/bootstrap/package-build-2.4/package-build-pkg.el
@@ -0,0 +1,11 @@
+(define-package "package-build" "2.4" "Tools for assembling a package archive"
+  '((cl-lib "0.5"))
+  :commit "bc06686806d676658b5e962497770e47025c17f1" :authors
+  '(("Donald Ephraim Curtis" . "dcurtis@milkbox.net"))
+  :maintainer
+  '("Donald Ephraim Curtis" . "dcurtis@milkbox.net")
+  :keywords
+  '("tools"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
