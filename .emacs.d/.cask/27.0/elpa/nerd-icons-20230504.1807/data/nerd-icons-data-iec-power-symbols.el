329a1ee4e20dfe5647b1d97c16848a87aa6e9be8
Abelardo Jara-Berrocal <abelardojarab@gmail.com>
2023-05-04 21:02:44 -0700

    add packages

---
+++ .emacs.d/.cask/27.0/elpa/nerd-icons-20230504.1807/data/nerd-icons-data-iec-power-symbols.el
@@ -0,0 +1,40 @@
+;;; nerd-icons-data-iec-power-symbols.el --- Nerd Font IEC Power Symbols -*- lexical-binding: t -*-
+
+;; Copyright (C) 2023 Hongyu Ding <rainstormstudio@yahoo.com>
+
+;; Author: Hongyu Ding <rainstormstudio@yahoo.com>
+;; Keywords: lisp
+;; Version: 0.0.1
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; IEC Power Symbols
+;; 23fb - 23fe
+;; 2b58
+
+;;; Code:
+
+(defvar nerd-icons/ipsicon-alist
+  '(
+    ("nf-iec-power" . "\x23fb")
+    ("nf-iec-power_off" . "\x2b58")
+    ("nf-iec-power_on" . "\x23fd")
+    ("nf-iec-sleep_mode" . "\x23fe")
+    ("nf-iec-toggle_power" . "\x23fc")
+    ))
+
+(provide 'nerd-icons-data-iec-power-symbols)
+;;; nerd-icons-data-iec-power-symbols.el ends here
---
+++ .emacs.d/.cask/27.0/elpa/nerd-icons-20230504.1807/nerd-icons-pkg.el
@@ -0,0 +1,14 @@
+(define-package "nerd-icons" "20230504.1807" "Emacs Nerd Font Icons Library"
+  '((emacs "24.3"))
+  :commit "1e0cc899ec2b565031b887b599501fe2776ad4a2" :authors
+  '(("Hongyu Ding <rainstormstudio@yahoo.com>, Vincent Zhang" . "seagle0128@gmail.com"))
+  :maintainers
+  '(("Hongyu Ding <rainstormstudio@yahoo.com>, Vincent Zhang" . "seagle0128@gmail.com"))
+  :maintainer
+  '("Hongyu Ding <rainstormstudio@yahoo.com>, Vincent Zhang" . "seagle0128@gmail.com")
+  :keywords
+  '("lisp")
+  :url "https://github.com/rainstormstudio/nerd-icons.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
