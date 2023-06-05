547eb873903e246aaf7db841dcf81f86f8a655af
Abelardo JaraBerrocal <abelardo.jaraberroca@getcruise.com>
2023-06-04 23:39:21 -0700

    marginalia was already present

--- .emacs.d/setup/setup-scroll.el
+++ .emacs.d/setup/setup-scroll.el
@@ -24,15 +24,14 @@
 
 ;;; Code:
 
-(if (display-graphic-p)
-    (setq-default scroll-step                     10
-                  scroll-margin                   3
-                  scroll-conservatively           101
-                  scroll-preserve-screen-position 't
-                  scroll-up-aggressively          0.01
-                  scroll-down-aggressively        0.01
-                  fast-but-imprecise-scrolling    t
-                  auto-window-vscroll             nil))
+(setq-default scroll-step                     10
+              scroll-margin                   3
+              scroll-conservatively           101
+              scroll-preserve-screen-position 't
+              scroll-up-aggressively          0.01
+              scroll-down-aggressively        0.01
+              fast-but-imprecise-scrolling    t
+              auto-window-vscroll             nil)
 
 (defadvice line-move-to-column (around bar activate)
   (ignore-errors add-do-it))
