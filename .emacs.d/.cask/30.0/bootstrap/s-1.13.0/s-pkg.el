60d205e43da79f0846c3784b7d1c101379ce2444
Abelardo Jara-Berrocal <abelardojarab@gmail.com>
2023-06-21 13:53:16 -0700

    fix
    Signed-off-by: Abelardo Jara-Berrocal <abelardojarab@gmail.com>

---
+++ .emacs.d/.cask/30.0/bootstrap/s-1.13.0/s-pkg.el
@@ -0,0 +1,11 @@
+(define-package "s" "1.13.0" "The long lost Emacs string manipulation library." 'nil :commit "4d7d83122850cf70dc60662a73124f0be41ad186" :authors
+  '(("Magnar Sveen" . "magnars@gmail.com"))
+  :maintainers
+  '(("Jason Milkins" . "jasonm23@gmail.com"))
+  :maintainer
+  '("Jason Milkins" . "jasonm23@gmail.com")
+  :keywords
+  '("strings"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
