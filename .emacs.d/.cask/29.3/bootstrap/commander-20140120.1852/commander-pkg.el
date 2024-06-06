7e0537a8d65e81e9488d9d71394b44584d13b0d5
Abelardo Jara-Berrocal <abelardojarab@gmail.com>
2024-06-05 21:07:43 -0700

    fix

---
+++ .emacs.d/.cask/29.3/bootstrap/commander-20140120.1852/commander-pkg.el
@@ -0,0 +1,17 @@
+(define-package "commander" "20140120.1852" "Emacs command line parser"
+  '((s "1.6.0")
+    (dash "2.0.0")
+    (cl-lib "0.3")
+    (f "0.6.1"))
+  :commit "2c8a57b9c619e29ccbe2d5a85921b9c689e95bf9" :authors
+  '(("Johan Andersson" . "johan.rejeep@gmail.com"))
+  :maintainers
+  '(("Johan Andersson" . "johan.rejeep@gmail.com"))
+  :maintainer
+  '("Johan Andersson" . "johan.rejeep@gmail.com")
+  :keywords
+  '("cli" "argv")
+  :url "http://github.com/rejeep/commander.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
